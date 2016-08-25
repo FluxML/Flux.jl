2b88be1c7f8ec8f57c695a2df3be7be111ab8ccc
Mike J Innes <mike.j.innes@gmail.com>
2016-08-26 00:18:58 +0100

    whoops

--- src/activation.jl
+++ src/activation.jl
@@ -6,6 +6,6 @@ back!(::typeof(σ), Δ, x) = Δ .* σ(x)./(1.-σ(x))
 
 relu(x) = max(0, x)
 
-back(::typeof(relu), Δ, x) = Δ .* (x .< 0)
+back!(::typeof(relu), Δ, x) = Δ .* (x .< 0)
 
 softmax(xs) = exp.(xs) ./ sum(exp.(xs))
