ba60c4596b4b2702d5d95723f39b18242f93108e
Mike J Innes <mike.j.innes@gmail.com>
2016-10-26 00:39:16 +0100

    graph op fixes

--- src/compiler/graph.jl
+++ src/compiler/graph.jl
@@ -2,16 +2,16 @@ immutable ModelInput end
 
 inputnode(n) = vertex(Split(n), constant(ModelInput()))
 
-# isinput(x) = isa(x, Constant) && value(x) == Input()
-
-# function bumpinput(v::IVertex)
-#   prewalk(v) do v
-#     isa(value(v), Split) && value(v[1]) == Input() ?
-#   end
-# end
+function bumpinputs(v::IVertex)
+  prewalk(v) do v
+    isa(value(v), Split) && value(v[1]) == Constant(ModelInput()) ?
+      inputnode(value(v).n + 1) :
+      v
+  end
+end
 
 function spliceinput(v::IVertex, input::IVertex)
-  prewalk(v) do v
+  postwalk(v) do v
     value(v) == Constant(ModelInput()) ? input : v
   end
 end
@@ -20,7 +20,7 @@ spliceinputs(v::IVertex, inputs::Vertex...) =
   spliceinput(v, vertex(Group(), inputs...))
 
 function detuple(v::IVertex)
-  prewalk(v) do v
+  postwalk(v) do v
     if isa(value(v), Split) && isa(value(v[1]), Group)
       v[1][value(v).n]
     else
