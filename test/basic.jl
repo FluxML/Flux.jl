39398680b75857d26ce4f39d3465c7ad6a10c19a
Mike J Innes <mike.j.innes@gmail.com>
2016-12-26 12:19:04 +0000

    test update

--- test/basic.jl
+++ test/basic.jl
@@ -5,6 +5,6 @@ d = Affine(10, 20)
 @test d(xs) == xs*d.W.x + d.b.x
 
 let
-  @capture(syntax(d), _Line(x_[1] * W_ + b_))
+  @capture(syntax(d), _Frame(_Line(x_[1] * W_ + b_)))
   @test isa(x, Input) && isa(W, Param) && isa(b, Param)
 end
