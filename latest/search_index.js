2f6fe6e126d8cf1155182468fa1f7be532f30756
autodocs <autodocs>
2017-01-16 01:04:23 +0000

    build based on 586f4d6

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Flux-1",
+    "page": "Home",
+    "title": "Flux",
+    "category": "section",
+    "text": ""
+},
+
+]}
---
+++ versions.js
@@ -0,0 +1,3 @@
+var DOC_VERSIONS = [
+  "latest",
+];
