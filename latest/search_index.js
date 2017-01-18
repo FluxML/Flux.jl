372f75560b866899f10eb6af921238df04c8b9c4
autodocs <autodocs>
2017-01-18 01:30:43 +0000

    build based on d86226c

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Flux",
     "category": "section",
-    "text": "Flux is a high-level interface for machine learning, implemented in Julia.Flux aims to be intuitive and powerful notation close to the mathematics, while providing advanced features like auto-unrolling and closures. Simple models are trivial, and the most complex architectures are tractable, taking orders of magnitude less code than in other frameworks. Meanwhile, the Flux compiler provides excellent error messages and tools for debugging when things go wrong.So what's the catch? Flux is at an early \"working prototype\" stage; many things work but the API is still in a state of- well, it might change.If you're interested to find out what does work, read on..."
+    "text": "Flux is a high-level interface for machine learning, implemented in Julia.Flux aims to be an intuitive and powerful notation, close to the mathematics, that provides advanced features like auto-unrolling and closures. Simple models are trivial, while the most complex architectures are tractable, taking orders of magnitude less code than in other frameworks. Meanwhile, the Flux compiler provides excellent error messages and tools for debugging when things go wrong.So what's the catch? Flux is at an early \"working prototype\" stage; many things work but the API is still in a state of... well, it might change.If you're interested to find out what does work, read on!"
 },
 
 {
