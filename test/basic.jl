e15a6e4896ae4c433d476880b1a118ce4e655432
Mike J Innes <mike.j.innes@gmail.com>
2017-02-01 18:23:28 +0530

    sub-model test

--- test/basic.jl
+++ test/basic.jl
@@ -8,3 +8,17 @@ let
   @capture(syntax(d), _Frame(_Line(x_[1] * W_ + b_)))
   @test isa(x, Input) && isa(W, Param) && isa(b, Param)
 end
+
+@net type TLP
+  first
+  second
+  function (x)
+    l1 = σ(first(x))
+    l2 = softmax(second(l1))
+  end
+end
+
+let a1 = Affine(10, 20), a2 = Affine(20, 15)
+  tlp = TLP(a1, a2)
+  @test tlp(xs) ≈ softmax(a2(σ(a1(xs))))
+end
