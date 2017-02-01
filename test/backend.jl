3b9ee7e2a87cc97d3984e60a4814e1a7767d4971
Mike J Innes <mike.j.innes@gmail.com>
2017-02-01 12:41:17 +0530

    remove tensorflow from ci

--- test/backend.jl
+++ test/backend.jl
@@ -1,19 +1,19 @@
 xs = rand(20)
 d = Affine(20, 10)
 
-let dt = tf(d)
+@tfonly let dt = tf(d)
   @test d(xs) ≈ dt(xs)
 end
 
-let dm = mxnet(d, (1, 20))
+@mxonly let dm = mxnet(d, (1, 20))
   @test d(xs) ≈ dm(xs)
 end
 
 # TensorFlow native integration
 
-using TensorFlow
+@tfonly let
+  using TensorFlow
 
-let
   sess = TensorFlow.Session()
   X = placeholder(Float32)
   Y = Tensor(d, X)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,14 @@ using DataFlow: Input, Line, Frame
 syntax(v::Vertex) = prettify(DataFlow.syntax(v))
 syntax(x) = syntax(graph(x))
 
+macro mxonly(ex)
+  :(Base.find_in_path("MXNet") ≠ nothing && $(esc(ex)))
+end
+
+macro tfonly(ex)
+  :(Base.find_in_path("TensorFlow") ≠ nothing && $(esc(ex)))
+end
+
 include("basic.jl")
 include("recurrent.jl")
 include("backend.jl")
