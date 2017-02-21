b2700920bc86a6cdddd963ffd9cc711705ff0de9
autodocs <autodocs>
2017-02-21 18:33:28 +0000

    build based on 8c1affd

--- latest/search_index.js
+++ latest/search_index.js
@@ -185,11 +185,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "apis/backends.html#Batching-1",
+    "location": "apis/backends.html#Backends-1",
     "page": "Backends",
-    "title": "Batching",
+    "title": "Backends",
     "category": "section",
-    "text": "[WIP]"
+    "text": ""
+},
+
+{
+    "location": "apis/backends.html#Basic-Usage-1",
+    "page": "Backends",
+    "title": "Basic Usage",
+    "category": "section",
+    "text": "model = Chain(Affine(10, 20), σ, Affine(20, 15), softmax)\nxs = rand(10)Currently, Flux's pure-Julia backend has no optimisations. This means that callingmodel(rand(10)) #> [0.0650, 0.0655, ...]directly won't have great performance. In order to support a computationally intensive training process, we really on a backend like MXNet or TensorFlow.This is easy to do. Just call either mxnet or tf on a model to convert it to a model of that kind:mxmodel = mxnet(model, (10, 1))\nmxmodel(xs) #> [0.0650, 0.0655, ...]\n# or\ntfmodel = tf(model)\ntfmodel(xs) #> [0.0650, 0.0655, ...]These new models look and feel exactly like every other model in Flux, including returning the same result when you call them, and can be trained as usual using Flux.train!(). The difference is that the computation is being carried out by a backend, which will usually give a large speedup."
+},
+
+{
+    "location": "apis/backends.html#Native-Integration-1",
+    "page": "Backends",
+    "title": "Native Integration",
+    "category": "section",
+    "text": "Flux aims to provide high-level APIs that work well across backends, but in some cases you may want to take advantage of features specific to a given backend. In these cases it's easy to \"drop down\" and use the backend's API directly, where appropriate. For example:using MXNet\nFlux.loadmx()\n\nmxmodel = mx.FeedForward(model)This returns a standard mx.FeedForward instance, just like you might have created using MXNet's usual API. You can then use this with MXNet's data provider implementation, custom optimisers, or distributed training processes.Same goes for TensorFlow, where it's easy to create a Tensor object:using TensorFlow\nFlux.loadtf()\n\nx  = placeholder(Float32)\ny = Tensor(model, x)This makes makes it easy to take advantage of Flux's model description and debugging tools while also getting the benefit of the work put into these backends. You can check out how this looks with the integration examples here."
 },
 
 {
