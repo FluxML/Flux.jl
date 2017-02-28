098268697897585f8cd3db29d5ec2f2c68dadc05
autodocs <autodocs>
2017-02-28 17:08:59 +0000

    build based on 2de6b06

--- latest/search_index.js
+++ latest/search_index.js
@@ -225,9 +225,9 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "apis/storage.html#Loading-and-Save-Models-1",
+    "location": "apis/storage.html#Loading-and-Saving-Models-1",
     "page": "Storing Models",
-    "title": "Loading and Save Models",
+    "title": "Loading and Saving Models",
     "category": "section",
     "text": "model = Chain(Affine(10, 20), σ, Affine(20, 15), softmax)Since models are just simple Julia data structures, it's very easy to save and load them using any of Julia's existing serialisation formats. For example, using Julia's built-in serialize:open(io -> serialize(io, model), \"model.jls\", \"w\")\nopen(io -> deserialize(io), \"model.jls\")One issue with serialize is that it doesn't promise compatibility between major Julia versions. For longer-term storage it's good to use a package like JLD.using JLD\n@save \"model.jld\" model\n@load \"model.jld\"However, JLD will break for some models as functions are not supported on 0.5+. You can resolve that by checking out this branch.Right now this is the only storage format Flux supports. In future Flux will support loading and saving other model formats (on an as-needed basis)."
 },
