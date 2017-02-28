a4156ffbc525b75f961980772c3a2b0dba012660
autodocs <autodocs>
2017-02-28 14:05:14 +0000

    build based on 50d3607

--- latest/search_index.js
+++ latest/search_index.js
@@ -245,7 +245,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Contributing & Help",
     "title": "Contributing",
     "category": "section",
-    "text": "[WIP]"
+    "text": "If you need help, please ask on the Julia forum or on Flux's Gitter.Right now, the best way to help out is to try out the examples and report any issues or missing features as you find them. The second best way is to help us spread the word, perhaps by starring the repo.If you're interested in hacking on Flux, most of the code is pretty straightforward. Adding new layer definitions or cost functions is simple using the Flux DSL itself, and things like data utilities and training processes are all plain Julia code. The compiler directory is a bit more involved and is documented in internals, but most changes won't need to touch that.If you get stuck or need anything, let us know!"
 },
 
 {
