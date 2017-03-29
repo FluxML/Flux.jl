5dde752f351b26d9e995cc7d82cce92e94c1ebd5
Mike J Innes <mike.j.innes@gmail.com>
2017-03-29 18:34:54 +0100

    maths test

--- test/recurrent.jl
+++ test/recurrent.jl
@@ -13,6 +13,7 @@ end
   r = Recurrent(10, 5)
   xs = [rand(10) for _ = 1:3]
   _, ys = apply(stateless(unroll1(r)), xs, (squeeze(r.y.x, 1),))
+  @test ys[1] == squeeze(tanh(reshape(xs[1],(1,10)) * r.Wxy.x .+ r.y.x * r.Wyy.x .+ r.by.x), 1)
   ru = unroll(r, 3)
   @test ru(Seq(xs)) == ys
 end
