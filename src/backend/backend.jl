e39c24fd8617734b3578446f199c22e35c377cee
Mike J Innes <mike.j.innes@gmail.com>
2017-04-17 15:56:59 +0100

    lazy loading note

--- src/backend/backend.jl
+++ src/backend/backend.jl
@@ -1,5 +1,14 @@
 export tf, mxnet
 
+# We use a lazy-loading trick to load the backend code as needed; this avoids
+# the need for a hard dependency on both backends.
+
+# This is effectively equivalent to:
+#   include("tensorflow/tensorflow.jl")
+#   using .TF
+#   export tf
+# but instead of loading immediately, we wait until `tf` is first called.
+
 function loadtf()
   isdefined(Flux, :TF) && return
   @eval include(joinpath(dirname($@__FILE__), "tensorflow/tensorflow.jl"))
