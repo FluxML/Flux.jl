88cf6d9e612cdf893a706d2d9607503b5a5f8094
Mike J Innes <mike.j.innes@gmail.com>
2017-04-18 20:55:59 +0100

    sensible default for unsqueeze

--- src/dims/utils.jl
+++ src/dims/utils.jl
@@ -1,4 +1,5 @@
-unsqueeze(xs, dim) = reshape(xs, (size(xs)[1:dim-1]..., 1, size(xs)[dim:end]...))
+unsqueeze(xs, dim = 1) = reshape(xs, (size(xs)[1:dim-1]..., 1, size(xs)[dim:end]...))
+Base.squeeze(xs) = squeeze(xs, 1)
 
 stack(xs, dim = 1) = cat(dim, unsqueeze.(xs, dim)...)
 unstack(xs, dim = 1) = [slicedim(xs, dim, i) for i = 1:size(xs, dim)]
