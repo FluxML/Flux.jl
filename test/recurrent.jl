f7f8124a789df850300db924d2573ad21e8aa526
Mike J Innes <mike.j.innes@gmail.com>
2017-04-27 17:27:46 +0100

    rm ambiguous function

--- test/recurrent.jl
+++ test/recurrent.jl
@@ -1,5 +1,3 @@
-using Flux: stateless
-
 function apply(model, xs, state)
   ys = similar(xs, 0)
   for x in xs
@@ -12,7 +10,7 @@ end
 @testset "RNN unrolling" begin
   r = Recurrent(10, 5)
   xs = [rand(1, 10) for _ = 1:3]
-  _, ys = apply(stateless(unroll1(r)), xs, (r.y.x,))
+  _, ys = apply(unroll1(r).model, xs, (r.y.x,))
   @test ys[1] == tanh(xs[1] * r.Wxy.x .+ r.y.x * r.Wyy.x .+ r.by.x)
   ru = unroll(r, 3)
   ru(batchone(Seq(squeeze.(xs))))[1] == squeeze.(ys)
