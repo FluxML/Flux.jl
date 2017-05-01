293460711590a03c67172f1cbb2b09586117d6d9
Mike J Innes <mike.j.innes@gmail.com>
2017-05-01 16:30:24 +0100

    multi-tuple output

--- src/backend/tensorflow/model.jl
+++ src/backend/tensorflow/model.jl
@@ -18,9 +18,12 @@ function makesession(model, n::Integer; session = Session(Graph()))
   makesession(model, [placeholder(Float32) for _ = 1:n], session = session)
 end
 
+retuple(xs) = xs
+retuple(xs::AbstractArray{<:AbstractArray}) = (retuple.(xs)...,)
+
 function (m::Exec)(args...)
   @assert length(args) == length(m.input)
-  run(m.session, m.output, Dict(zip(m.input, args)))
+  retuple(run(m.session, m.output, Dict(zip(m.input, args))))
 end
 
 mutable struct Model
