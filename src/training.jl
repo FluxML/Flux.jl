33c6781c996a5943e67709c94b6a43eb2048cff6
Mike J Innes <mike.j.innes@gmail.com>
2017-05-01 13:47:08 +0100

    remove from training.jl

--- src/training.jl
+++ src/training.jl
@@ -1,17 +1,3 @@
-tobatch(xs::Batch) = rawbatch(xs)
-tobatch(xs) = tobatch(batchone(xs))
-
-function accuracy(m, data)
-  n = 0
-  correct = 0
-  for (x, y) in data
-    x, y = tobatch.((x, y))
-    n += size(x, 1)
-    correct += sum(onecold(m(x)) .== onecold(y))
-  end
-  return correct/n
-end
-
 """
   @cb for ... end t expr
 
