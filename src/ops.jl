268895474cbf358f2bd094a5051ac52d8302c25d
Ali Hamdi <alha02@outlook.com>
2017-05-11 13:23:09 +0200

    add julia-op fill which takes dims as array, consistent with tf op

--- src/ops.jl
+++ src/ops.jl
@@ -1,3 +1,6 @@
-export tile
+export tile, fill
+
+import Base: fill
 
 tile(x::AbstractArray, mult::AbstractArray) = repeat(x,outer=tuple(mult...))
+fill{T}(x::T, dims::AbstractArray) = fill(x,tuple(dims...))
