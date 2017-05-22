3532c7174f9a167740154018b1d95e8c86af6079
Mike J Innes <mike.j.innes@gmail.com>
2017-05-22 17:32:10 +0100

    early throw on cyclic graphs

--- src/compiler/interp.jl
+++ src/compiler/interp.jl
@@ -15,6 +15,7 @@ end
 
 function interp(ctx, f, xs...)
   g = graph(f)
+  g ≠ nothing && iscyclic(g) && error("Can't interpret cyclic graph")
   @icatch(ctx, g ≠ nothing ?
     interpret(ctx, reifyparams(g), xs...) :
     f(xs...))
