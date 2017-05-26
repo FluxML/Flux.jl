7bcd0133cc8545601249f15f011d29e4deaebfd4
Ali Hamdi <alha02@outlook.com>
2017-05-26 20:17:55 +0200

    test for fill and replace abs with abs.

--- test/backend/tensorflow.jl
+++ test/backend/tensorflow.jl
@@ -32,27 +32,27 @@ error_margin = 1e-6
 xs = convert(Array{Float32},randn(6,3))
 @net f(x,y) = reshape(x,y)
 m = tf(f)
-@test maximum(abs(reshape(xs,(9,2)) - transpose(m(transpose(xs),[2,9])))) < error_margin # Note: TF is row major and julia is not
+@test maximum(abs.(reshape(xs,(9,2)) - transpose(m(transpose(xs),[2,9])))) < error_margin # Note: TF is row major and julia is not
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # transpose
 @net f(x) = transpose(x)
 m = tf(f)
-@test maximum(abs(m(xs)-transpose(xs))) < error_margin
+@test maximum(abs.(m(xs)-transpose(xs))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # permutedims
 xs = convert(Array{Float32},randn(6,3,2))
 @net f(x,y) = permutedims(x,y)
 m = tf(f)
-@test maximum(abs(m(xs,[3,2,1])-permutedims(xs,[3,2,1]))) < error_margin
+@test maximum(abs.(m(xs,[3,2,1])-permutedims(xs,[3,2,1]))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # tile
 xs = convert(Array{Float32},randn(4,3,1))
 @net f(x,y) = tile(x,y)
 m = tf(f)
-@test maximum(abs(m(xs,[1,1,3])-repeat(xs,outer=(1,1,3)))) < error_margin
+@test maximum(abs.(m(xs,[1,1,3])-repeat(xs,outer=(1,1,3)))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # chol
@@ -60,14 +60,14 @@ z = convert(Array{Float32},randn(6,5))
 z = z'*z
 @net f(x) = chol(x)
 m = tf(f)
-@test maximum(abs(m(z)-chol(z))) < error_margin
+@test maximum(abs.(m(z)-chol(z))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # size
 z = randn(4,5)
 @net f(x) = size(x)
 m = tf(f)
-@test maximum(abs(m(z)-[size(z)...])) == 0
+@test maximum(abs.(m(z)-[size(z)...])) == 0
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # cat
@@ -75,7 +75,13 @@ z1 = convert(Array{Float32},randn(4,1))
 z2 = convert(Array{Float32},randn(4,1))
 @net f(x,y) = cat(2,x,y)
 m = tf(f)
-@test maximum(abs(m(z1,z2)-cat(2,z1,z2))) < error_margin
+@test maximum(abs.(m(z1,z2)-cat(2,z1,z2))) < error_margin
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# fill
+@net f(x,y) = fill(x,y)
+m = tf(f)
+@test maximum(abs.(m(3.2,[3,2])-convert(Array{Float32},3.2*ones(3,2)))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
