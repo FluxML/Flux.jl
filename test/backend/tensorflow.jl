0bec9fff5635a9ff7e6a64017c7a880efca44e05
Ali Hamdi <alha02@outlook.com>
2017-05-26 18:48:31 +0200

    add test for tile,chol,size

--- test/backend/tensorflow.jl
+++ test/backend/tensorflow.jl
@@ -25,26 +25,49 @@ end
 @testset "Ops" begin
 
 error_margin = 1e-6
+#using Flux,Base.Test
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # reshape
 xs = convert(Array{Float32},randn(6,3))
 @net f(x,y) = reshape(x,y)
 m = tf(f)
-@test maximum(reshape(xs,(9,2)) - transpose(m(transpose(xs),[2,9]))) < error_margin # Note: TF is row major and julia is not
+@test maximum(abs(reshape(xs,(9,2)) - transpose(m(transpose(xs),[2,9])))) < error_margin # Note: TF is row major and julia is not
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # transpose
 @net f(x) = transpose(x)
 m = tf(f)
-@test maximum(m(xs)-transpose(xs)) < error_margin
+@test maximum(abs(m(xs)-transpose(xs))) < error_margin
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # permutedims
 xs = convert(Array{Float32},randn(6,3,2))
 @net f(x,y) = permutedims(x,y)
 m = tf(f)
-@test maximum(m(xs,[3,2,1])-permutedims(xs,[3,2,1])) < error_margin
+@test maximum(abs(m(xs,[3,2,1])-permutedims(xs,[3,2,1]))) < error_margin
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# tile
+xs = convert(Array{Float32},randn(4,3,1))
+@net f(x,y) = tile(x,y)
+m = tf(f)
+@test maximum(abs(m(xs,[1,1,3])-repeat(xs,outer=(1,1,3)))) < error_margin
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# chol
+z = convert(Array{Float32},randn(6,5))
+z = z'*z
+@net f(x) = chol(x)
+m = tf(f)
+@test maximum(abs(m(z)-chol(z))) < error_margin
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# size
+z = randn(4,5)
+@net f(x) = size(x)
+m = tf(f)
+@test maximum(abs(m(z)-[size(z)...])) == 0
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
