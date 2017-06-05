f0880f89ccd9d8875d7ee70b76115411031f849a
Mike J Innes <mike.j.innes@gmail.com>
2017-06-05 19:16:24 +0100

    fix

--- test/batching.jl
+++ test/batching.jl
@@ -10,7 +10,7 @@ bs = Batch([[1,2,3],[4,5,6]])
 
 batchseq = Batch([Seq([[1,2,3],[4,5,6]]),Seq([[7,8,9],[10,11,12]])])
 
-@test batchseq == [[[1,2,3],[4,5,6]]]
+@test batchseq == [[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]
 @test rawbatch(batchseq)[1,1,3] == 3
 @test rawbatch(batchseq)[2,2,1] == 10
 
