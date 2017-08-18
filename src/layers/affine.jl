21089fea9c64dcecfaee237148da5e1ef1f5e618
Mike J Innes <mike.j.innes@gmail.com>
2017-08-18 01:18:07 +0100

    remove some explicit back!

--- src/layers/affine.jl
+++ src/layers/affine.jl
@@ -6,16 +6,3 @@ end
 
 Affine(in::Integer, out::Integer; init = initn) =
   Affine(init(in, out), init(1, out))
-
-function back!(m::Affine, Δ, x)
-  W, b = m.W, m.b
-  W.Δx[:] = x' * Δ
-  b.Δx[:] = sum(Δ, 1)
-  Δ * W.x'
-end
-
-function update!(m::Affine, η)
-  update!(m.W, η)
-  update!(m.b, η)
-  m
-end
