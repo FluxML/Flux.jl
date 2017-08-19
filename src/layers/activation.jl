0222103c7f8ac33b57fa4f4cd7261f3ec3c8664b
Mike J Innes <mike.j.innes@gmail.com>
2017-08-19 20:05:22 +0100

    fix these later

--- src/layers/activation.jl
+++ src/layers/activation.jl
@@ -1,10 +1,10 @@
 # Sigmoid
 σ(x) = 1 ./ (1 + exp.(-x))
-back!(::typeof(σ), Δ, x) = Δ .* σ(x).*(1.-σ(x))
+# back!(::typeof(σ), Δ, x) = Δ .* σ(x).*(1.-σ(x))
 
 # Rectified Linear Unit
 relu(x) = max(0, x)
-back!(::typeof(relu), Δ, x) = Δ .* (x .> 0)
+# back!(::typeof(relu), Δ, x) = Δ .* (x .> 0)
 
 softmax(xs) = exp.(xs) ./ sum(exp.(xs), 2)
 
--- src/layers/cost.jl
+++ src/layers/cost.jl
@@ -1,7 +1,7 @@
 mse(ŷ, y) = sumabs2(ŷ .- y)/2
 
-back!(::typeof(mse), Δ, ŷ, y) = Δ .* (ŷ .- y)
+# back!(::typeof(mse), Δ, ŷ, y) = Δ .* (ŷ .- y)
 
 logloss(ŷ, y) = -sum(y .* log.(ŷ))
 
-back!(::typeof(logloss), Δ, ŷ, y) = 0 .- Δ .* y ./ ŷ
+# back!(::typeof(logloss), Δ, ŷ, y) = 0 .- Δ .* y ./ ŷ
