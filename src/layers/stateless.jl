18e69b33c95154e0ea1bf15e6cf76f4d18dc907d
Mike J Innes <mike.j.innes@gmail.com>
2017-08-19 22:05:50 +0100

    forwarddiff does these

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -1,10 +1,8 @@
 # Activation Functions
 
 σ(x) = 1 / (1 + exp(-x))
-# back!(::typeof(σ), Δ, x) = Δ .* σ(x).*(1.-σ(x))
 
 relu(x) = max(0, x)
-# back!(::typeof(relu), Δ, x) = Δ .* (x .> 0)
 
 softmax(xs) = exp.(xs) ./ sum(exp.(xs), 2)
 
