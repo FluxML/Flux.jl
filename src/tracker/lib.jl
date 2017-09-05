830d7fa611fe0bf805b4bd1b46f81144630fb6a5
Mike J Innes <mike.j.innes@gmail.com>
2017-09-05 02:28:11 -0400

    vcat fix

--- src/tracker/lib.jl
+++ src/tracker/lib.jl
@@ -32,9 +32,9 @@ Base.vcat(a::TrackedVector, b::TrackedVector)  = TrackedArray(Call(vcat, a, b))
 Base.vcat(a::TrackedVector, b::AbstractVector) = TrackedArray(Call(vcat, a, b))
 Base.vcat(a::AbstractVector, b::TrackedVector) = TrackedArray(Call(vcat, a, b))
 
-Base.vcat(a::TrackedArray, b::TrackedArray)  = TrackedArray(Call(vcat, a, b))
-Base.vcat(a::TrackedArray, b::AbstractArray) = TrackedArray(Call(vcat, a, b))
-Base.vcat(a::AbstractArray, b::TrackedArray) = TrackedArray(Call(vcat, a, b))
+Base.vcat(a::TrackedMatrix, b::TrackedMatrix)  = TrackedArray(Call(vcat, a, b))
+Base.vcat(a::TrackedMatrix, b::AbstractMatrix) = TrackedArray(Call(vcat, a, b))
+Base.vcat(a::AbstractMatrix, b::TrackedMatrix) = TrackedArray(Call(vcat, a, b))
 
 function back!(::typeof(vcat), Δ, xs, ys)
   i = Base.tail(map(_ -> :, size(Δ)))
