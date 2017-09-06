3ef72a9d7b19b48fa01e089a3af889704b93ca72
Mike J Innes <mike.j.innes@gmail.com>
2017-09-06 18:59:43 -0400

    utils updates

--- src/compiler/loops.jl
+++ src/compiler/loops.jl
@@ -1,8 +1,4 @@
-unsqueeze(xs, dim = 1) = reshape(xs, (size(xs)[1:dim-1]..., 1, size(xs)[dim:end]...))
-squeeze(xs, dim = 1) = Base.squeeze(xs, dim)
-
-stack(xs, dim = 1) = cat(dim, unsqueeze.(xs, dim)...)
-unstack(xs, dim = 1) = [slicedim(xs, dim, i) for i = 1:size(xs, dim)]
+using ..Flux: stack, unstack, squeeze, unsqueeze
 
 # Stateful Models
 
