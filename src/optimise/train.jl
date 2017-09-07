085d3aa9b4c0ccc5b88db00a21dee70fd2855941
Mike J Innes <mike.j.innes@gmail.com>
2017-09-07 00:29:55 -0400

    handle epoch elsewhere

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -4,14 +4,11 @@ using Flux.Tracker: back!
 tocb(f) = f
 tocb(fs::AbstractVector) = () -> foreach(call, fs)
 
-function train!(m, data, opt; epoch = 1, cb = () -> ())
+function train!(m, data, opt; cb = () -> ())
   cb = tocb(cb)
-  @progress for e in 1:epoch
-    epoch > 1 && info("Epoch $e")
-    @progress for (x, y) in data
-      back!(m(x, y))
-      opt()
-      cb()
-    end
+  @progress for (x, y) in data
+    back!(m(x, y))
+    opt()
+    cb()
   end
 end
