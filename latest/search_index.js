40abfd3683f135410e3bb7ba8124308ba149428a
autodocs <autodocs>
2017-09-07 05:42:18 +0000

    build based on 706baab

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,6 +13,30 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Flux",
     "category": "section",
+    "text": "Flux is a library for machine learning, implemented in Julia. Flux lets you use the full power of the Julia language while providing a set of useful helper functionality, like common mathematical functions and optimisers."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Install Julia 0.6.0 or later, if you haven't already.Pkg.add(\"Flux\")\nPkg.test(\"Flux\") # Check things installed correctlyStart with the basics. The model zoo is also a good starting point for many common kinds of models."
+},
+
+{
+    "location": "basics.html#",
+    "page": "Basics",
+    "title": "Basics",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "recurrence.html#",
+    "page": "Recurrence",
+    "title": "Recurrence",
+    "category": "page",
     "text": ""
 },
 
@@ -29,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Contributing & Help",
     "title": "Contributing",
     "category": "section",
-    "text": "If you need help, please ask on the Julia forum, the slack (channel #machine-learning), or Flux's Gitter.Right now, the best way to help out is to try out the examples and report any issues or missing features as you find them. The second best way is to help us spread the word, perhaps by starring the repo.If you're interested in hacking on Flux, most of the code is pretty straightforward. Adding new layer definitions or cost functions is simple using the Flux DSL itself, and things like data utilities and training processes are all plain Julia code. The compiler directory is a bit more involved and is documented in internals, but most changes won't need to touch that.If you get stuck or need anything, let us know!"
+    "text": "If you need help, please ask on the Julia forum, the slack (channel #machine-learning), or Flux's Gitter.Right now, the best way to help out is to try out the examples and report any issues or missing features as you find them. The second best way is to help us spread the word, perhaps by starring the repo.If you're interested in hacking on Flux, most of the code is pretty straightforward. Adding new layer definitions or cost functions is simple using the Flux DSL itself, and things like data utilities and training processes are all plain Julia code.If you get stuck or need anything, let us know!"
 },
 
 ]}
