aeaa138b6d83844862ec3d8ada8ad9e2f6140737
Mike J Innes <mike.j.innes@gmail.com>
2017-09-07 00:27:16 -0400

    cb convenience

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -1,7 +1,11 @@
 using Juno
 using Flux.Tracker: back!
 
+tocb(f) = f
+tocb(fs::AbstractVector) = () -> foreach(call, fs)
+
 function train!(m, data, opt; epoch = 1, cb = () -> ())
+  cb = tocb(cb)
   @progress for e in 1:epoch
     epoch > 1 && info("Epoch $e")
     @progress for (x, y) in data
