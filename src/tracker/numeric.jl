a93c440c1e27e54eeaf2059a3fc9be553aa864f7
Mike J Innes <mike.j.innes@gmail.com>
2017-09-06 23:09:39 -0400

    style

--- src/tracker/numeric.jl
+++ src/tracker/numeric.jl
@@ -6,17 +6,15 @@ end
 
 function ngradient(f, xs::AbstractArray...)
   grads = zeros.(xs)
-  for (x, Δ) in zip(xs, grads)
-    for i in 1:length(x)
-      δ = sqrt(eps())
-      tmp = x[i]
-      x[i] = tmp - δ/2
-      y1 = f(xs...)
-      x[i] = tmp + δ/2
-      y2 = f(xs...)
-      x[i] = tmp
-      Δ[i] = (y2-y1)/δ
-    end
+  for (x, Δ) in zip(xs, grads), i in 1:length(x)
+    δ = sqrt(eps())
+    tmp = x[i]
+    x[i] = tmp - δ/2
+    y1 = f(xs...)
+    x[i] = tmp + δ/2
+    y2 = f(xs...)
+    x[i] = tmp
+    Δ[i] = (y2-y1)/δ
   end
   return grads
 end
