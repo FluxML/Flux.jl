0b095579a1fd22a95ec080aad284620c7b721640
Mike J Innes <mike.j.innes@gmail.com>
2017-09-07 01:11:43 -0400

    strip old docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,23 +7,10 @@ makedocs(modules=[Flux],
          sitename = "Flux",
          assets = ["../flux.css"],
          pages = ["Home" => "index.md",
-                  "Building Models" => [
-                    "Model Building Basics" => "models/basics.md",
-                    "Model Templates"       => "models/templates.md",
-                    "Recurrence"            => "models/recurrent.md",
-                    "Debugging"             => "models/debugging.md"],
-                  "Other APIs" => [
-                    "Batching" => "apis/batching.md",
-                    "Backends" => "apis/backends.md",
-                    "Storing Models" => "apis/storage.md"],
-                  "In Action" => [
-                    "Simple MNIST" => "examples/logreg.md",
-                    "Char RNN" => "examples/char-rnn.md"],
-                  "Contributing & Help"   => "contributing.md",
-                  "Internals" => "internals.md"])
+                  "Contributing & Help"   => "contributing.md"])
 
 deploydocs(
-   repo = "github.com/MikeInnes/Flux.jl.git",
+   repo = "github.com/FluxML/Flux.jl.git",
    target = "build",
    osname = "linux",
    julia = "0.6",
--- docs/src/internals.md
+++
@@ -1,3 +0,0 @@
-# Internals
-
-[WIP]
