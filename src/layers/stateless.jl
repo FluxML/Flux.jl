d32b67f5c63d506fc960212c3ab303e23de538da
pevnak <pevnak@gmail.com>
2017-09-26 11:28:04 +0200

    added crossentropy and its weighted variant

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -5,3 +5,11 @@ mse(ŷ, y) = sum((ŷ .- y).^2)/length(y)
 
 logloss(ŷ, y) = -sum(y .* log.(ŷ)) / size(y, 2)
 # back!(::typeof(logloss), Δ, ŷ, y) = 0 .- Δ .* y ./ ŷ
+
+"""
+    crossentropy(logŷ,y,w)
+
+    implement the cross-entropy loss function taking the logits (output before the softmax)
+"""
+crossentropy(logŷ, y) = crossentropy(logŷ, y, 1/size(y,2))
+crossentropy(logŷ, y, w) = -sum(y .*(logŷ .- log.( sum( exp.( logŷ),1))))
