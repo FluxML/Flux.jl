a0ee62b06443bf56bdaa941505fea97985bd3d86
pevnak <pevnak@gmail.com>
2017-09-26 15:45:01 +0200

    fixed the cross-entropy

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -7,9 +7,14 @@ logloss(ŷ, y) = -sum(y .* log.(ŷ)) / size(y, 2)
 # back!(::typeof(logloss), Δ, ŷ, y) = 0 .- Δ .* y ./ ŷ
 
 """
+    crossentropy(logŷ, y)
     crossentropy(logŷ,y,w)
 
     implement the cross-entropy loss function taking the logits (output before the softmax)
 """
 crossentropy(logŷ, y) = crossentropy(logŷ, y, 1/size(y,2))
-crossentropy(logŷ, y, w) = -sum(y .*(logŷ .- log.( sum( exp.( logŷ),1))))
+function crossentropy(logŷ, y, w) 
+  logŷ = logŷ .-maximum(logŷ,1)
+  ypred = logŷ .- log.( sum( exp.( logŷ),1))
+  -sum(y .* w .* ypred)
+end
