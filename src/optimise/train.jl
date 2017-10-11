bc9a818317cfc7f791a1f235fff91231c7d00dff
CarloLucibello <carlo.lucibello@gmail.com>
2017-10-11 13:18:50 +0200

    change argument name for better clarity

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -4,10 +4,10 @@ using Flux.Tracker: back!
 tocb(f) = f
 tocb(fs::AbstractVector) = () -> foreach(call, fs)
 
-function train!(m, data, opt; cb = () -> ())
+function train!(loss, data, opt; cb = () -> ())
   cb = tocb(cb)
   @progress for x in data
-    l = m(x...)
+    l = loss(x...)
     isinf(l.data[]) && error("Loss is Inf")
     isnan(l.data[]) && error("Loss is NaN")
     back!(l)
