4aa7741ba9b291d9846d55dc520b6982a6dd1aae
pevnak <pevnak@gmail.com>
2017-10-17 17:57:10 +0100

    logit cross entropy

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -6,3 +6,9 @@ crossentropy(ŷ::AbstractVecOrMat, y::AbstractVecOrMat) =
   -sum(y .* log.(ŷ)) / size(y, 2)
 
 @deprecate logloss(x, y) crossentropy(x, y)
+
+function logitcrossentropy(logŷ, y::AbstractMatrix, w)
+  logŷ = logŷ .-maximum(logŷ,1)
+  ypred = logŷ .- log.( sum( exp.( logŷ),1))
+  -sum(y .* w .* ypred)
+end
