949fd9ba97238dc93f89868363a51d78f05b0c6e
Mike J Innes <mike.j.innes@gmail.com>
2017-10-17 17:30:11 +0100

    loss function tweaks

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -1,7 +1,6 @@
 # Cost functions
 
 mse(ŷ, y) = sum((ŷ .- y).^2)/length(y)
-# back!(::typeof(mse), Δ, ŷ, y) = Δ .* (ŷ .- y)
 
-logloss(ŷ, y) = -sum(y .* log.(ŷ)) / size(y, 2)
-# back!(::typeof(logloss), Δ, ŷ, y) = 0 .- Δ .* y ./ ŷ
+logloss(ŷ::AbstractVecOrMat, y::AbstractVecOrMat) =
+  -sum(y .* log.(ŷ)) / size(y, 2)
