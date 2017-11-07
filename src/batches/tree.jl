752a9e2808884f56b6c9f8f3a8a4706f407692ea
Mike J Innes <mike.j.innes@gmail.com>
2017-11-07 10:57:27 +0000

    tree utilities

--- src/batches/tree.jl
+++ src/batches/tree.jl
@@ -13,6 +13,9 @@ Tree(x::T, xs::Tree{T}...) where T = Tree{T}(x, xs...)
 AbstractTrees.children(t::Tree) = t.children
 AbstractTrees.printnode(io::IO, t::Tree) = show(io, t.value)
 
+Base.show(io::IO, t::Type{Tree}) = print(io, "Tree")
+Base.show(io::IO, t::Type{Tree{T}}) where T = print(io, "Tree{", T, "}")
+
 function Base.show(io::IO, t::Tree)
   println(io, typeof(t))
   print_tree(io, t)
@@ -24,3 +27,16 @@ using Juno
   render(t) = Juno.Tree(t.value, render.(t.children))
   Juno.Tree(typeof(t), [render(t)])
 end
+
+Base.getindex(t::Tree, i::Integer) = t.children[i]
+Base.getindex(t::Tree, i::Integer, is::Integer...) = t[i][is...]
+
+# Utilities
+
+isleaf(t) = isempty(children(t))
+
+leaves(xs::Tree) = map(x -> x.value, Leaves(xs))
+
+Base.map(f, t::Tree, ts::Tree...) =
+  Tree{Any}(f(map(t -> t.value, (t, ts...))...),
+            [map(f, chs...) for chs in zip(map(t -> t.children, (t, ts...))...)]...)
