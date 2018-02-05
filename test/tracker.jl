f9be72f5454c2669fc0f81c671cf7cbae2de08a4
Mike J Innes <mike.j.innes@gmail.com>
2018-02-05 18:50:59 +0000

    logsoftmax tests

--- test/tracker.jl
+++ test/tracker.jl
@@ -17,10 +17,8 @@ gradtest(f, dims...) = gradtest(f, rand.(dims)...)
 
 @test gradtest(x -> softmax(x).*(1:3), 3)
 @test gradtest(x -> softmax(x).*(1:3), (3,5))
-
-## uncomment the following test when logsoftmax has been added into NNlib.jl
-#@test gradtest(x -> logsoftmax(x).*(1:3), 3)
-#@test gradtest(x -> logsoftmax(x).*(1:3), (3,5))
+@test gradtest(x -> logsoftmax(x).*(1:3), 3)
+@test gradtest(x -> logsoftmax(x).*(1:3), (3,5))
 
 @test gradtest(Flux.mse, rand(5,5), rand(5, 5))
 @test gradtest(Flux.crossentropy, rand(5,5), rand(5, 5))
