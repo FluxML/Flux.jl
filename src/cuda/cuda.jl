7ac9e191cbd2d9fb235d48bd023178c70778f7e5
Avik Pal <avikpal@iitk.ac.in>
2018-06-28 14:25:22 +0530

    Revert 1 change

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -3,7 +3,6 @@ module CUDA
 using CuArrays
 
 if CuArrays.cudnn_available()
-    CuParam{T,N} = Union{CuArray{T,N},TrackedArray{T,N,CuArray{T,N}}}
     include("curnn.jl")
     include("cudnn.jl")
 end
