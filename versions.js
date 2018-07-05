8681c63a9ef7350809514bd834d6e3cc3805f643
autodocs <autodocs>
2018-07-05 11:36:42 +0000

    build based on ce88273

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.5",
   "release-0.4",
   "release-0.3",
   "release-0.2",
   "release-0.1",
+  "v0.5.3",
   "v0.5.0",
   "v0.4.1",
   "v0.4.0",
