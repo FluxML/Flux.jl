e14641e4e283815fa9ee2c70426ba5d91db7a534
Mike J Innes <mike.j.innes@gmail.com>
2018-07-12 20:48:40 +0100

    rm CuArrays tests for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,8 @@ include("layers/stateless.jl")
 include("optimise.jl")
 include("data.jl")
 
-if Base.find_in_path("CuArrays") ≠ nothing
-  include("cuda/cuda.jl")
-end
+# if Base.find_in_path("CuArrays") ≠ nothing
+#   include("cuda/cuda.jl")
+# end
 
 end
