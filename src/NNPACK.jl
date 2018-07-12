5e727e7539b85f6fda31851f005e37403468b822
americast <sayan.sinha@iitkgp.ac.in>
2018-07-13 03:08:33 +0530

    Fix chain

--- src/NNPACK.jl
+++ src/NNPACK.jl
@@ -1,11 +1,25 @@
+using NNlib: NNPACK
+
+struct Conv{N,F,A,V}
+  σ::F
+  weight::A
+  bias::V
+  stride::NTuple{N,Int}
+  pad::NTuple{N,Int}
+  dilation::NTuple{N,Int}
+end
+
 function (c::Conv)(x)
   # TODO: breaks gpu broadcast :(
   # ndims(x) == ndims(c.weight)-1 && return squeezebatch(c(reshape(x, size(x)..., 1)))
   σ, b = c.σ, reshape(c.bias, map(_->1, c.stride)..., :, 1)
-
-  if σ == Flux.relu
-  	NNlib.NNPACK.conv(x, c.weight, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 1, bias = c.bias)
+  wt = copy(c.weight.data)
+  @show σ
+  if σ == NNlib.relu
+  	println("Here")
+  	NNlib.NNPACK.convo(x, wt, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 1)
   else
-  	σ.(NNlib.NNPACK.conv(x, c.weight, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 0, bias = c.bias))
+  	println("There")
+  	σ.(NNlib.NNPACK.convo(x, wt, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 0))
   end
 end
