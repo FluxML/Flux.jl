c3180265811df952fd21f5ef95e686bfcb223f0c
americast <sayan.sinha@iitkgp.ac.in>
2018-07-13 10:35:37 +0530

    better tracking

--- src/NNPACK.jl
+++ src/NNPACK.jl
@@ -1,4 +1,6 @@
+import NNlib.conv
 using NNlib: NNPACK
+using Flux: Tracker
 
 struct Conv{N,F,A,V}
   σ::F
@@ -9,17 +11,31 @@ struct Conv{N,F,A,V}
   dilation::NTuple{N,Int}
 end
 
+_conv(x, w, stride, pad, dilation, activation, bias) = NNlib.NNPACK.convo(x, w, bias, stride = stride, pad = pad, dilation = dilation, activation = activation)
+
+conv(x::TrackedArray{<:Real,N}, w::TrackedArray{<:Real,N}, bias; stride = 1, pad = 0, dilation = 1, activation = 0) where N =
+  Tracker.track(_conv, x, w, stride, pad, dilation, activation, bias)
+conv(x::AbstractArray{<:Real,N}, w::TrackedArray{<:Real,N}, bias; stride = 1, pad = 0, dilation = 1, activation = 0) where N =
+  Tracker.track(_conv, x, w, stride, pad, dilation, activation, bias)
+conv(x::TrackedArray{<:Real,N}, w::AbstractArray{<:Real,N}, bias; stride = 1, pad = 0, dilation = 1, activation = 0) where N =
+  Tracker.track(_conv, x, w, stride, pad, dilation, activation, bias)
+
 function (c::Conv)(x)
   # TODO: breaks gpu broadcast :(
   # ndims(x) == ndims(c.weight)-1 && return squeezebatch(c(reshape(x, size(x)..., 1)))
   σ, b = c.σ, reshape(c.bias, map(_->1, c.stride)..., :, 1)
-  wt = copy(c.weight.data)
   # @show σ
   if σ == NNlib.relu
   	# println("Here")
-  	NNlib.NNPACK.convo(x, wt, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 1)
+  	conv(x, c.weight, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 1)
   else
   	println("There")
-  	σ.(NNlib.NNPACK.convo(x, wt, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 0))
+  	σ.(conv(x, c.weight, c.bias, stride = c.stride, pad = c.pad, dilation = c.dilation, activation = 0))
   end
+end
+
+
+function back(::typeof(_conv), Δ, x, w, stride, pad, dilation)
+  Tracker.@back(x, NNlib.NNPACK.∇conv_data(Δ, data(x), data(w); stride = stride, pad = pad, dilation = dilation))
+  Tracker.@back(w, NNlib.NNPACK.∇conv_filter(Δ, data(x), data(w); stride = stride, pad = pad, dilation = dilation))
 end
