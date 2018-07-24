97b0959ca9476ce059986f32dda4c9386c828156
Avik Pal <avikpal@iitk.ac.in>
2018-07-24 15:28:45 +0530

    Typos

--- src/cuda/cudnn.jl
+++ src/cuda/cudnn.jl
@@ -354,12 +354,12 @@ function convbias!(y::CuArray{T}, x::CuArray{T}, w::CuArray{T}, b::CuArray{T};
   cudnnConvolutionBiasActivationForward(y, x, w, b, padding=pad, stride=stride, mode=mode, alpha1=alpha, activationMode=activationMode)
 end
 
-function convbias(x::CuArray{T}, w::CuArray{T}, b::CuArray{T}, λ;
+function convbias(x::CuArray{T}, w::CuArray{T}, b::CuArray{T};
                   pad = 0, stride = 1, mode = 0,
                   alpha = 1, dilation = 1) where T<:Union{Float32,Float64}
   pad_, stride_ = padtuple(x, pad), padtuple(x, stride)
   convbias!(similar(x, cdims(size(x), dilation_dims(w, dilation), pad_, stride_)),
-            x, w, b, pad = pad_, stride = stride_, dilation = dilation, activationMode = activationMode)
+            x, w, b, pad = pad_, stride = stride_, dilation = dilation, activationMode = 5)
 end
 
 ∇conv_bias(Δ::CuArray{T}, b::CuArray{T}; pad = 0, beta = 0,
@@ -371,7 +371,7 @@ function (m::Flux.Conv)(x::Union{CuParam{T,4},CuParam{T,5}})  where T<:Union{Flo
   m.σ == identity ? result : m.σ.(result) # Replace with cudnn function calls
 end
 
-convbias(x::TrackedArray, w::TrackedArray, b::TrackedArray; kw...) = track(convbias, x, w, b, λ; kw...)
+convbias(x::TrackedArray, w::TrackedArray, b::TrackedArray; kw...) = track(convbias, x, w, b; kw...)
 
 convbias(x::CuArray{T}, w::TrackedArray, b::TrackedArray; kw...) where T<:Union{Float32,Float64} =
   track(convbias, x, w, b, kw...)
@@ -393,7 +393,7 @@ convbias(x::TrackedArray, w::CuArray{T}, b::CuArray{T}; kw...) where T<:Union{Fl
 
 @grad function convbias(x, w, b; kw...)
   bias = reshape(b, map(_->1, kw[2][2])..., :, 1)
-  y = convbias(data.((x, w, bias))..., λ; kw...)
+  y = convbias(data.((x, w, bias))...; kw...)
   y, Δ -> (istracked(x) ? NNlib.∇conv_data(data.((Δ, x, w))...; kw...) : nothing,
            istracked(w) ? NNlib.∇conv_filter(data.((Δ, x, w))...; kw...) : nothing,
            istracked(b) ? ∇conv_bias(data.((Δ, bias)); kw...) : nothing)
