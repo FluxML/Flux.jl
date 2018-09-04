1e90226077457249af527f69d9fb6018f21dc2e4
Mike J Innes <mike.j.innes@gmail.com>
2018-09-04 14:35:20 +0100

    actually run tests

--- test/layers/conv.jl
+++ test/layers/conv.jl
@@ -1,8 +1,7 @@
-using Test
-using Flux: Chain, Conv, MaxPool, MeanPool, maxpool, meanpool
-using Base.conv
+using Flux, Test
+using Flux: maxpool, meanpool
 
-@testset "pooling" begin
+@testset "Pooling" begin
   x = randn(10, 10, 3, 2)
   mp = MaxPool((2, 2))
   @test mp(x) == maxpool(x, (2,2))
@@ -10,17 +9,15 @@ using Base.conv
   @test mp(x) == meanpool(x, (2,2))
 end
 
-@testset "cnn" begin
-    r = zeros(28, 28)
-    m = Chain(
-      Conv((2, 2), 1=>16, relu),
-      MaxPool{2}(2),
-      Conv((2, 2), 16=>8, relu),
-      MaxPool{2}(2),
-      x -> reshape(x, :, size(x, 4)),
-      Dense(288, 10), softmax)
+@testset "CNN" begin
+  r = zeros(28, 28, 1, 5)
+  m = Chain(
+    Conv((2, 2), 1=>16, relu),
+    MaxPool((2,2)),
+    Conv((2, 2), 16=>8, relu),
+    MaxPool((2,2)),
+    x -> reshape(x, :, size(x, 4)),
+    Dense(288, 10), softmax)
 
-  @testset "inference" begin
-    @test size(m(r)) == (10, )
-  end
+  @test size(m(r)) == (10, 5)
 end
