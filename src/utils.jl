61fb6cdf053da66f29f1afb3161f8a86434b0572
Mike Innes <mike.j.innes@gmail.com>
2018-10-05 14:02:00 +0100

    jit macro

--- src/utils.jl
+++ src/utils.jl
@@ -153,3 +153,18 @@ function jacobian(m,x)
     end
     J'
 end
+
+"""
+    @jit ...
+
+The `@jit` annotation can be applied to any code, and the code will be compiled
+for performance.
+
+    @jit f(x) = @jit(x) + @jit(x)
+
+Note that compilation happens regardless of the `@jit` macro, so it should only
+be used for aesthetic purposes, or by recovering Python users.
+"""
+macro jit(ex)
+  esc(ex)
+end
