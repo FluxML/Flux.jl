db4db19169fa9b74896da2e5d59e0f577dcb1b3b
Avik Pal <avikpal@iitk.ac.in>
2018-12-01 19:47:32 +0530

    Update the function calls

--- src/cuda/cudnn.jl
+++ src/cuda/cudnn.jl
@@ -1,6 +1,6 @@
 using .CuArrays.CUDNN: @check, libcudnn, cudnnStatus_t, cudnnTensorDescriptor_t,
   cudnnBatchNormMode_t, cudnnHandle_t, cudnnDataType, TensorDesc, FilterDesc,
-  CUDNN_VERSION
+  version, CUDNNFloat
 import ..Flux: data
 using LinearAlgebra
 
@@ -46,14 +46,14 @@ BNCache() = BNCache(nothing, nothing)
 batchnorm(g::CuArray{T}, b::CuArray{T}, x::CuArray{T, 2},
           running_mean::CuArray{T}, running_var::CuArray{T}, momentum;
           cache = nothing, alpha = T(1), beta = T(0),
-          eps = T(1e-5), training = true) where T<:Union{Float32, Float64} =
+          eps = T(1e-5), training = true) where T<:CUDNNFloat =
   dropdims(batchnorm(g, b, reshape(x, 1, 1, size(x, 1), size(x, 2)), running_mean, running_var, momentum,
             cache = cache, alpha = alpha, beta = beta, eps = eps, training = training), dims = (1, 2))
 
 function batchnorm(g::CuArray{T}, b::CuArray{T}, x::Union{CuArray{T, 4},CuArray{T,5}},
                    running_mean::CuArray{T}, running_var::CuArray{T}, momentum;
                    cache = nothing, alpha = T(1), beta = T(0),
-                   eps = T(1e-5), training = true) where T<:Union{Float32, Float64}
+                   eps = T(1e-5), training = true) where T<:CUDNNFloat
   y = similar(x)
   cudnnBNForward!(y, g, b, x, running_mean, running_var, momentum, cache = cache,
       alpha = alpha, beta = beta, eps = eps, training = training)
@@ -64,7 +64,7 @@ function cudnnBNForward!(y::CuArray{T}, g::CuArray{T}, b::CuArray{T}, x::CuArray
                         running_mean::CuArray{T}, running_var::CuArray{T},
                         momentum; cache = nothing,
                         alpha = T(1), beta = T(0),
-                        eps = T(1e-5), training = true) where T<:Union{Float32, Float64}
+                        eps = T(1e-5), training = true) where T<:CUDNNFloat
   dims = _wsize(x)
   if eps < BATCHNORM_MIN_EPS
     # warn("eps ",eps," is too small for CuDNN so eps has been assigned the value ", BATCHNORM_MIN_EPS)
@@ -126,7 +126,7 @@ end
 function ∇batchnorm(g::CuArray{T}, b::CuArray{T}, x::CuArray{T, 2}, dy::CuArray{T, 2},
            running_mean::CuArray{T}, running_var::CuArray{T}, momentum;
            cache = nothing, eps = T(1e-5), alpha = T(1),
-           beta = T(0), training = true) where T<:Union{Float32, Float64}
+           beta = T(0), training = true) where T<:CUDNNFloat
   dg, db, dx = ∇batchnorm(g, b, reshape(x, 1, 1, size(x, 1), size(x, 2)), reshape(dy, 1, 1, size(dy, 1),
                           size(dy, 2)), running_mean, running_var, momentum, cache = cache, eps = eps,
                           alpha = alpha, beta = beta, training = training)
@@ -136,7 +136,7 @@ end
 function ∇batchnorm(g::CuArray{T}, b::CuArray{T}, x::CuArray{T}, dy::CuArray{T},
                     running_mean::CuArray{T}, running_var::CuArray{T}, momentum;
                     cache = nothing, eps = T(1e-5), alpha = T(1),
-                    beta = T(0), training = true) where T<:Union{Float32, Float64}
+                    beta = T(0), training = true) where T<:CUDNNFloat
   dg = similar(g)
   db = similar(b)
   dx = similar(x)
@@ -150,7 +150,7 @@ function cudnnBNBackward!(dg::CuArray{T}, g::CuArray{T}, db::CuArray{T},
                           running_mean::CuArray{T}, running_var::CuArray{T},
                           momentum; cache = nothing, eps = T(1e-5),
                           alpha = T(1), beta = T(0),
-                          dalpha = T(1), dbeta = T(0), training = true) where T<:Union{Float32, Float64}
+                          dalpha = T(1), dbeta = T(0), training = true) where T<:CUDNNFloat
   if training
     xd = TensorDesc(x)
     dyd = TensorDesc(dy)
@@ -195,94 +195,110 @@ end
 # Flux Interface
 
 import Flux.Tracker
-import Flux.Tracker: data, istracked, track, unbroadcast, @grad, nobacksies, track_kw
+import Flux.Tracker: stracked, track, unbroadcast, @grad, nobacksies
 using NNlib: padtuple, cdims, dilation_dims, conv, ∇conv_data, ∇conv_filter
-using CuArrays.CUDNN: cudnnConvolutionBackwardBias, cudnnConvolutionBackwardData, cudnnConvolutionBackwardFilter,
-  cudnnActivationBackward,  cudnnConvolutionBiasActivationForward, cudnnAddTensor
+using CuArrays.CUDNN: conv_workspace, cudnnConvolutionBackwardBias, cudnnConvolutionBackwardData,
+  cudnnConvolutionBackwardFilter, cudnnActivationBackward, cudnnAddTensor,
+  cudnnGetConvolutionForwardWorkspaceSize, cudnnConvolutionBiasActivationForward
 
-(BN::Flux.BatchNorm)(x::Union{CuParam{T,2},CuParam{T,4},CuParam{T,5}}, cache = nothing) where T<:Union{Float32, Float64} =
+(BN::Flux.BatchNorm)(x::Union{CuParam{T,2},CuParam{T,4},CuParam{T,5}}, cache = nothing) where T<:CUDNNFloat =
   batchnorm(BN.γ, BN.β, x, BN.μ, BN.σ², BN.momentum; cache = cache, alpha = 1, beta = 0, eps = BN.ϵ, training = BN.active)
 
 batchnorm(g::TrackedArray, b::TrackedArray, x::TrackedArray, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::TrackedArray, b::TrackedArray, x::CuArray{T}, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::TrackedArray, b::CuArray{T}, x::TrackedArray, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::CuArray{T}, b::TrackedArray, x::CuArray{T}, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::CuArray{T}, b::TrackedArray, x::TrackedArray, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::TrackedArray, b::CuArray{T}, x::CuArray{T}, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 batchnorm(g::CuArray{T}, b::CuArray{T}, x::TrackedArray, running_mean::CuArray{T},
-          running_var::CuArray{T}, momentum; kw...) where T<:Union{Float32, Float64} =
+          running_var::CuArray{T}, momentum; kw...) where T<:CUDNNFloat =
   track(batchnorm, g, b, x, running_mean, running_var, momentum; kw...)
 
 @grad batchnorm(g, b, x, running_mean, running_var, momentum; kw...) =
   batchnorm(data.((g, b, x))..., running_mean, running_var, momentum; kw...), Δ -> (nobacksies(:batchnorm, ∇batchnorm(data.((g, b, x, Δ))..., running_mean, running_var, momentum; kw...))..., nothing, nothing, nothing)
 
 function convbias!(y::CuArray{T}, x::CuArray{T}, w::CuArray{T}, b::CuArray{T};
-                   pad = 0, stride = 1, mode = 0,
-                   alpha = 1, dilation = 1, activationMode = 5) where T<:Union{Float32,Float64}
-  all(x -> x == 1, dilation) || error("Only dilation = 1 is supported in CuArrays")
-  cudnnConvolutionBiasActivationForward(y, x, w, b, padding=pad, stride=stride, mode=mode, alpha1=alpha, activationMode=activationMode)
+                   pad = 0, stride = 1, flipkernel = 0, alpha = 1, dilation = 1,
+                   workspace::Union{CuVector, Nothing}=nothing, algo=0,
+                   activationMode = 5) where T<:CUDNNFloat
+  if version() < v"6"
+    all(x -> x == 1, dilation) || error("Only dilation = 1 is supported in cuDNN version < 6")
+  end
+  if workspace === nothing
+    workspace_size =
+      cudnnGetConvolutionForwardWorkspaceSize(y, x, w, padding=pad, stride=stride, dilation=dilation,
+                                              algo=algo, mode=flipkernel)
+    workspace = workspace_size != 0 ? conv_workspace(workspace_size) : workspace
+  else
+    workspace_size = length(workspace[])
+  end
+  cudnnConvolutionBiasActivationForward(y, x, w, b, padding=pad, stride=stride, mode=flipkernel, alpha1=alpha, activationMode=activationMode, algo=algo, workspace=workspace, workspace_size=workspace_size)
 end
 
 function convbias(x::CuArray{T}, w::CuArray{T}, b::CuArray{T};
-                  pad = 0, stride = 1, mode = 0,
-                  alpha = 1, dilation = 1) where T<:Union{Float32,Float64}
+                  pad = 0, stride = 1, flipkernel = 0, alpha = 1, dilation = 1,
+                  workspace::Union{CuVector, Nothing}=nothing, algo=0,
+                  activationMode = 5) where T<:CUDNNFloat
   pad_, stride_ = padtuple(x, pad), padtuple(x, stride)
   convbias!(similar(x, cdims(size(x), dilation_dims(w, dilation), pad_, stride_)),
-            x, w, b, pad = pad_, stride = stride_, dilation = dilation, activationMode = 5)
+            x, w, b, pad = pad_, stride = stride_, flipkernel = flipkernel, dilation = dilation,
+            alpha = alpha, workspace = workspace, algo = algo, activationMode = activationMode)
 end
 
 ∇conv_bias(Δ::CuArray{T}, b::CuArray{T}; pad = 0, beta = 0,
-           stride = 1, mode = 0, alpha = 1, dilation = 1) where T<:Union{Float32,Float64} =
+           stride = 1, mode = 0, alpha = 1, dilation = 1) where T<:CUDNNFloat =
   reshape(cudnnConvolutionBackwardBias(similar(b), Δ, alpha=alpha, beta=beta), :)
 
-function (m::Flux.Conv)(x::Union{CuParam{T,4},CuParam{T,5}})  where T<:Union{Float32,Float64}
-  # Will cause a problem in cudnn version < 7.1 if none of x, weight and bias are tracked
-  result = convbias(x, m.weight, m.bias, pad = m.pad, stride = m.stride, dilation = m.dilation)
-  m.σ == identity ? result : m.σ.(result) # Replace with cudnn function calls
-end
+(m::Flux.Conv)(x::Union{CuParam{T,4},CuParam{T,5}})  where T<:CUDNNFloat =
+  m.σ.(convbias(x, m.weight, m.bias, pad = m.pad, stride = m.stride, dilation = m.dilation))
 
-convbias(x::TrackedArray, w::TrackedArray, b::TrackedArray; kw...) = track_kw(convbias, x, w, b; kw...)
+convbias(x::TrackedArray, w::TrackedArray, b::TrackedArray; kw...) = track(convbias, x, w, b; kw...)
 
-convbias(x::CuArray{T}, w::TrackedArray, b::TrackedArray; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::CuArray{T}, w::TrackedArray, b::TrackedArray; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
-convbias(x::CuArray{T}, w::CuArray{T}, b::TrackedArray; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::CuArray{T}, w::CuArray{T}, b::TrackedArray; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
-convbias(x::TrackedArray, w::CuArray{T}, b::TrackedArray; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::TrackedArray, w::CuArray{T}, b::TrackedArray; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
-convbias(x::TrackedArray, w::TrackedArray, b::CuArray{T}; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::TrackedArray, w::TrackedArray, b::CuArray{T}; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
-convbias(x::CuArray{T}, w::TrackedArray, b::CuArray{T}; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::CuArray{T}, w::TrackedArray, b::CuArray{T}; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
-convbias(x::TrackedArray, w::CuArray{T}, b::CuArray{T}; kw...) where T<:Union{Float32,Float64} =
-  track_kw(convbias, x, w, b; kw...)
+convbias(x::TrackedArray, w::CuArray{T}, b::CuArray{T}; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
+
+convbias(x::TrackedArray, w::TrackedArray, b::CuArray{T}; kw...) where T<:CUDNNFloat =
+  track(convbias, x, w, b; kw...)
 
 @grad function convbias(x, w, b; kw...)
   bias = reshape(b, map(_->1, kw[2][2])..., :, 1)
-  CUDNN_VERSION >= 7100 ? convbias(data.((x, w, bias))...; kw...) : cudnnAddTensor(data(bias), conv(data.((x, w))...; kw...)),
-    Δ -> (istracked(x) ? ∇conv_data(data.((Δ, x, w))...; kw...) : nothing,
-          istracked(w) ? ∇conv_filter(data.((Δ, x, w))...; kw...) : nothing,
-          istracked(b) ? ∇conv_bias(data.((Δ, bias))...; kw...) : nothing)
+  if version() >= v"7.1"
+    y = convbias(data.((x, w, bias))...; kw...)
+  else
+    y = cudnnAddTensor(data(bias), conv(data.((x, w))...; kw...))
+  end
+  y, Δ -> (nobacksies(:convbias, ∇conv_data(data.((Δ, x, w))...; kw...), ∇conv_filter(data.((Δ, x, w))...; kw...), ∇conv_bias(data.((Δ, bias))...; kw...))
 end
