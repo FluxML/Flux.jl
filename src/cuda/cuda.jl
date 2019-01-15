9d56807bcd32461547dc8c4c0b3e8ef90057c2b8
Mike J Innes <mike.j.innes@gmail.com>
2019-01-15 11:43:57 -0500

    cuarrays version check

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -1,6 +1,22 @@
 module CUDA
 
 using ..CuArrays
+using Pkg.TOML
+
+function version_check()
+  minor_version = 9
+  project = joinpath(dirname(pathof(CuArrays)), "../Project.toml")
+  project = TOML.parse(String(read(project)))
+  version = VersionNumber(get(project, "version", "0.0.0"))
+  if !(version.major == 0 && version.minor == minor_version)
+    @warn """
+    Flux is only supported with CuArrays v0.$minor_version.
+    Try running `] pin CuArrays@0.$minor_version`.
+    """
+  end
+end
+
+version_check()
 
 if !applicable(CuArray{UInt8}, undef, 1)
   (T::Type{<:CuArray})(::UndefInitializer, sz...) = T(sz...)
