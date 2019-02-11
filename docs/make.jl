3786b3c84a3e4b431765e41935bd1efcbbffe558
Lyndon White <oxinabox@ucc.asn.au>
2019-02-11 13:12:04 +0000

    add perf tips

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ makedocs(modules=[Flux, NNlib],
                   "One-Hot Encoding" => "data/onehot.md",
                   "GPU Support" => "gpu.md",
                   "Saving & Loading" => "saving.md",
+                  "Performance Tips" => "performance_tips.md",
                   "Internals" =>
                     ["Backpropagation" => "internals/tracker.md"],
                   "Community" => "community.md"])
