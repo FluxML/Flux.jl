395604643c32e30a78999a0da79bcf2feb6384a0
swiesend <sebastian@wiesendahl.de>
2019-02-27 00:59:27 +0100

    add inv to Parallel layer

--- src/layers/parallel.jl
+++ src/layers/parallel.jl
@@ -8,7 +8,17 @@ end
 # see https://github.com/keras-team/keras/blob/master/keras/layers/wrappers.py#L333
 
 function concat(xs)
-    vcat(xs...)
+    # implicit concatenation
+    return vcat(xs...)
+
+    # explcit concatenation
+    # n = length(xs)
+    # m = length(xs[1])
+    # concated = Array{eltype(xs[1].data)}(UndefInitializer(), n * m)
+    # for i in 1:n
+    #     concated[(i-1)*m+1:i*m] = xs[i].data
+    # end
+    # return param(concated)
 end
 
 function mul(xs)
@@ -19,34 +29,33 @@ function mul(xs)
     D
 end
 
-function mean(xs)
-    D = sum(xs) ./ length(xs)
-    D
-end
+# function mean(xs)
+#     D = sum(xs) ./ length(xs)
+#     D
+# end
 
 
-mutable struct Parallel  #{M<:Recur}
+mutable struct Parallel
     layers::Vector
     map::Vector{Function}
     inv::Vector{Function}
     reduce::Function
 end
 
-function Parallel(layers::Vector)
-    Parallel(layers, identities(length(layers)), identities(length(layers)), concat)
-end
 
 function Parallel(
     layers::Vector;
     map::Dict{Int64,Function} = Dict{Int64,Function}(),
-    reduce::Function = concat) # where M<:Recur
-
-    # TODO: throw error for min length for layers - 1 or 2?
+    inv::Dict{Int64,Function} = Dict{Int64,Function}(),
+    reduce::Function = concat)
 
     mappings::Vector{Function} = identities(length(layers))
-    inverses::Vector{Function} = copy(mappings)
     for (k,v) in map
         mappings[k] = v
+    end
+
+    inverses::Vector{Function} = identities(length(layers))
+    for (k,v) in inv
         inverses[k] = v
     end
 
@@ -58,23 +67,23 @@ function (p::Parallel)(xs)
     #       How to parallelize for cpu() and gpu() ia an open question to me, as `Base.pmap` does not exist anymore and 
     #       `Threads.@threads` does not seem to be a good idea neither.
 
-    # double reverse, analog to `Flux.flip` but without broadcast; see: recurrent.jl#flip(f, xs)
+    # double reverse; analog to `Flux.flip`, but without broadcast; see: recurrent.jl#flip(f, xs)
     # y = map^-1(f(map(x))) or map(x) |> f |> map^-1
     apply(l) = p.inv[l](p.layers[l](p.map[l](xs)))
     
     # implicit mapping
-    # Z = map(l -> apply(l), eachindex(p.layers))
+    Z = map(l -> apply(l), eachindex(p.layers))
 
     # explicit mapping - define type and size of Z    
-    first = apply(1)
-    Z = Vector{typeof(first)}(UndefInitializer(), length(p.layers))
-    for l in eachindex(p.layers)
-        if l == 1
-            Z[l] = first
-        else
-            Z[l] = apply(l)
-        end
-    end
+    # first = apply(1)
+    # Z = Vector{typeof(first)}(UndefInitializer(), length(p.layers))
+    # for l in eachindex(p.layers)
+    #     if l == 1
+    #         Z[l] = first
+    #     else
+    #         Z[l] = apply(l)
+    #     end
+    # end
 
     p.reduce(Z)
 end
@@ -84,78 +93,117 @@ end
 Flux.children(p::Parallel) = p.layers
 Flux.mapchildren(f, p::Parallel) = Parallel(f.(p.layers), p.map, p.inv, p.reduce)
 
-Base.show(io::IO, m::Parallel) = print(io, "Parallel(", m.layers, ", ", m.map, ", ", m.inv, ", ", m.reduce, ")")
-
-function _truncate_parallel(x)
-    if x isa Recur
-        x.state = _truncate(x.state)
-    elseif x isa Parallel
-        for layer in x.layers
-            _truncate_parallel(layer)
-        end
-    end
+function Base.show(io::IO, m::Parallel)
+    print(io, "Parallel(\n")
+    print(io, "  layers = ", m.layers, ",\n")
+    print(io, "     map = ", m.map, ",\n")
+    print(io, "     inv = ", m.inv, ",\n")
+    print(io, "  reduce = ", m.reduce, "\n")
+    print(io, ")")
 end
 
-function truncate_parallel!(m)
-    prefor(_truncate_parallel, m)
-end
 
-function _reset_parallel(x)
-    if x isa Recur
-        x.state = x.init
-    elseif x isa Parallel
-        for recur in x.layers
-            _reset_parallel(recur)
-        end
-    end
-end
-
-function reset_parallel!(m)
-    prefor(_reset_parallel, m)
-end
-
-function Base.reverse(M::Flux.OneHotMatrix{Array{Flux.OneHotVector,1}})
-    # Flux.OneHotMatrix(M.height, reverse(M.data))
-    # M.data = reverse(M.data)
+"""
     reverse(M)
+    
+Reverse the input vector for each batch.
+
+# Examples
+```julia-repl
+julia> M = onehotbatch([:a, :a, :b, :b], [:a, :b, :c])
+3×4 Flux.OneHotMatrix{Array{Flux.OneHotVector,1}}:
+  true   true  false  false
+ false  false   true   true
+ false  false  false  false
+
+julia> reverse(M)
+3×4 Flux.OneHotMatrix{Array{Flux.OneHotVector,1}}:
+ false  false   true   true
+  true   true  false  false
+ false  false  false  false
+```
+"""
+function Base.reverse(M::Flux.OneHotMatrix{Array{Flux.OneHotVector,1}})
+    Flux.OneHotMatrix(M.height, reverse(M.data))
 end
 
+"""
+    reverse(v)
+
+Return the identity of the one-hot vector.
+"""
 function Base.reverse(v::Flux.OneHotVector)
     v
 end
 
-function Base.reverse(ta::TrackedArray)
+"""
+    reverse(x)
+
+Reverse a tracked array or matrix.
+
+# Examples
+```julia-repl
+
+julia> a = param([1, 2, 3, 4, 5, 6])
+Tracked 6-element Array{Float64,1}:
+ 1.0
+ 2.0
+ 3.0
+ 4.0
+ 5.0
+ 6.0
+
+julia> reverse(a)
+Tracked 6-element Array{Float64,1}:
+ 6.0
+ 5.0
+ 4.0
+ 3.0
+ 2.0
+ 1.0
+
+julia> T = param([1 2 3; 4 5 6])
+Tracked 2×3 Array{Float64,2}:
+ 1.0  2.0  3.0
+ 4.0  5.0  6.0
+
+julia> reverse(T)
+Tracked 2×3 Array{Float64,2}:
+ 3.0  2.0  1.0
+ 6.0  5.0  4.0
+```
+"""
+function Base.reverse(ta::Flux.TrackedArray; dims=2)
     if length(size(ta.data)) == 2
-        # TODO: does this need to be param() or params() or how to propagate the tracking?
-        # reverse(ta.data, dims=2)
-        # param(reverse(ta.data, dims=2))
-        reverse(ta)
+        param(reverse(ta.data, dims=dims))
     else
-        ta
+        param(reverse(ta.data))
     end
 end
 
-function Base.reverse(b::Bool)
-    b
-end
+"""
+    reverse(v)
 
-function Base.reverse(x::Number)
+Reversing a tracked number returns the identity of the number.
+"""
+function Base.reverse(x::Flux.Tracker.TrackedReal)
     x
 end
+
 
 # see:
 #  "SPEECH RECOGNITION WITH DEEP RECURRENT NEURAL NETWORKS" https://arxiv.org/pdf/1303.5778.pdf
 #  "Bidirectional LSTM-CRF Models for Sequence Tagging" https://arxiv.org/pdf/1508.01991.pdf
 #  Bidirectional layer of Keras: https://github.com/keras-team/keras/blob/05d713504852b490afcf2607aea1ce923e93ecfe/keras/layers/wrappers.py#L333
 function Bi(layer::Recur, reduce::Function = concat)
-    map = Dict{Int64,Function}(2 => reverse)
-    Parallel([layer, deepcopy(layer)], map=map, reduce=reduce)
+    mapping = Dict{Int64,Function}(2 => reverse)
+    Parallel([layer, deepcopy(layer)], map=mapping, inv=mapping, reduce=reduce)
 end
 
-function BiLSTM(in::Int, out::Int; reduce::Function = concat)
+function BiLSTM(in::Int, out::Int, reduce::Function = concat)
     Bi(LSTM(in, out), reduce)
 end
 
-function BiPLSTM(in::Int, out::Int; reduce::Function = concat)    
+function BiPLSTM(in::Int, out::Int, reduce::Function = concat)    
     Bi(PLSTM(in, out), reduce)
 end
