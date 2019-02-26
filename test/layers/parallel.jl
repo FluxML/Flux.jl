4fe7e53d20cb03cf7799316cec8bde23c99b4896
swiesend <sebastian@wiesendahl.de>
2019-02-27 00:59:51 +0100

    run test for all models on the same data

--- test/layers/parallel.jl
+++ test/layers/parallel.jl
@@ -6,51 +6,55 @@ using Base.Iterators: partition
 
 @testset "Parallel" begin
 
-    @testset "one lonely `LSTM()` in a `Parallel` layer - should behave like a single `LSTM()`" begin
-        
-        data = collect(partition(rand(10, 7), 10))
+    data = collect(partition(rand(10, 7), 10))
 
-        # Parallel map/reduce
+    models = [
+        # non recurrent layers
+        Chain(Parallel([Dense(10,10), Dense(10,10)]), Dense(20,10)),
 
-        ## non recurrent
-        # m = Chain(Parallel([Dense(10,10), Dense(10,10)]), Dense(20,10))
+        # recurrent layers - reduce defaults to `Flux.concat: 10 -> 10
+        Parallel([LSTM(10,10)]),
+        Chain(Parallel([LSTM(10,10)])),
 
-        ## recurrent
-        # m = Parallel([LSTM(10,10)])
-        # m = Chain(Parallel([LSTM(10,10)]))
+        # for reduce see also: `sum`, `mean`, `Flux.mul`
+        Parallel([LSTM(10,10), LSTM(10,10)], reduce=sum),
+        Chain(Parallel([LSTM(10,10), LSTM(10,10)], reduce=mean)),
 
-        # FIXME: DimensionMismatch("arrays could not be broadcast to a common size")
-        # m = Parallel([LSTM(10,10), LSTM(10,10)])  
-        # m = Chain(Parallel([LSTM(10,10), LSTM(10,10)]))
+        # reduce can be `Flux.concat` -> reduction is effectifly done by a Dense layer
+        Chain(Parallel([LSTM(10,10)]), Dense(10,10)),
+        Chain(Parallel([LSTM(10,10), LSTM(10,10)]), Dense(20,10)),
 
-        m = Chain(Parallel([LSTM(10,10)]), Dense(10,10))
-        # m = Chain(Parallel([LSTM(10,10),LSTM(10,10)]), Dense(20,10))
-        
-        ## bidirectional LSTM
-        # FIXME: DimensionMismatch("arrays could not be broadcast to a common size")
-        # m = Bi(LSTM(10, 10))
-        # m = Chain(BiLSTM(10,10))
+        # bidirectional LSTM
+        Parallel([LSTM(10,10), LSTM(10,10)],
+            map = Dict{Int64,Function}(2 => reverse),
+            inv = Dict{Int64,Function}(2 => reverse),
+            reduce = sum),
 
-        # m = Chain(BiLSTM(10,10), Dense(20,10))  # default: reduce=Flux.concat
-        # m = Chain(BiLSTM(10,10, reduce=sum), Dense(10,10))
-        # m = Chain(BiLSTM(10,10, reduce=Flux.mul), Dense(10,10))
-        # m = Chain(BiLSTM(10,10, reduce=Flux.mean), Dense(10,10))
+        # BiLSTM - a convenience layer, which makes use of `Parallel` and the MapReduce approach
+        # for reduce see also: `sum`, `mean`, `Flux.mul`
+        Bi(LSTM(10, 10), sum),
+        Chain(BiLSTM(10,10, sum)),
 
-        ## peephole LSTM
-        # m = Chain(PLSTM(10,10))
-        # m = Chain(PLSTM(10,10), Dense(10,10))
-        # m = Chain(BiPLSTM(10,10), Dense(20,10))
-        # m = Chain(BiPLSTM(10,10), BiPLSTM(20,10), Dense(20,10))
+        # peephole LSTM - an modified LSTM layer commonly used in image processing.
+        Chain(PLSTM(10,10)),
+        Chain(PLSTM(10,10), Dense(10,10)),
 
-        # @show m
-        # @show params(m)
+        # BiPLSTM - a convenience layer, which makes use of `Parallel` and the MapReduce approach
+        Chain(BiPLSTM(10,10), Dense(20,10)),
+        Chain(BiPLSTM(10,10), BiPLSTM(20,10), Dense(20,10)),
+    ]
+
+    @testset "models using a `Parallel` layer" for (i,m) in enumerate(models)
+        println("\n\ntest ($i)\n")
+        @show m
+        sleep(0.1)
 
         before = Flux.data(m(data[1]))
         @test length(before) == 10 || length(before) == 20
 
         function loss(x, y)
             l = mse(m(x), y)
-            Flux.truncate_parallel!(m)
+            Flux.truncate!(m)
             l
         end
 
@@ -61,22 +65,14 @@ using Base.Iterators: partition
         opt = ADAM()
         @epochs 3 Flux.train!(loss, params(m), zip(data, data), opt, cb = evalcb)
 
-        Flux.reset_parallel!(m)
+        Flux.reset!(m)
         after = Flux.data(m(data[1]))
         @test length(before) == length(after[:,end]) || length(before) == 2 * length(after[:,end])
         @test before != after[:,end]
 
-        Flux.reset_parallel!(m)
+        Flux.reset!(m)
         after = Flux.data(m(data[1]))
         @test before != after
     end
-
-    # @testset "reverse input for second layer" begin
-    #     m = Parallel(layers, map = Dict{Int64,Function}(2 => reverse))
-    # end
-
-    # @testset "bidirectional layers with average merge" begin
-    #     m = Parallel(layers, map = Dict{Int64,Function}(2 => reverse), reduce = mean)
-    # end
 
 end
