2db5aa265fbf77f3fcfdb40bbe497774a80f0206
Naman Biyani <namanb@iitk.ac.in>
2019-03-05 11:42:07 +0530

    Update normalise.jl

--- src/layers/normalise.jl
+++ src/layers/normalise.jl
@@ -16,28 +16,28 @@ _testmode!(m, test) = nothing
     Dropout(p)
 
 A Dropout layer. For each input, either sets that input to `0` (with probability
-`prob`) or scales it by `1/(1-prob)`. This is used as a regularisation, i.e. it
+`p`) or scales it by `1/(1-p)`. This is used as a regularisation, i.e. it
 reduces overfitting during training.
 
 Does nothing to the input once in [`testmode!`](@ref).
 """
 mutable struct Dropout{F}
-  prob::F
+  p::F
   active::Bool
 end
 
-function Dropout(prob)
-  @assert 0 ≤ prob ≤ 1
-  Dropout{typeof(prob)}(prob, true)
+function Dropout(p)
+  @assert 0 ≤ p ≤ 1
+  Dropout{typeof(p)}(p, true)
 end
 
-_dropout_kernel(y::T, prob, q) where {T} = y > prob ? T(1 / q) : T(0)
+_dropout_kernel(y::T, p, q) where {T} = y > p ? T(1 / q) : T(0)
 
 function (a::Dropout)(x)
   a.active || return x
   y = similar(x)
   rand!(y)
-  y .= _dropout_kernel.(y, a.prob, 1 - a.prob)
+  y .= _dropout_kernel.(y, a.p, 1 - a.p)
   return x .* y
 end
 
@@ -66,9 +66,9 @@ function Base.show(io::IO, l::LayerNorm)
 end
 
 """
-    BatchNorm(channels::Integer, σ(variance) = identity;
+    BatchNorm(channels::Integer, running_var(σ²) = identity;
               initβ(bias) = zeros, initγ(scale) = ones,
-              ϵ = 1e-8, momentum = .1)
+              ϵ(eps) = 1e-8, momentum = .1)
 
 Batch Normalization layer. The `channels` input should be the size of the
 channel dimension in your data (see below).
@@ -98,17 +98,17 @@ mutable struct BatchNorm{F,V,W,N}
   activation_function::F  # λ
   bias::V  # β
   scale::V  #γ
-  moving_mean::W  # μ
-  moving_std::W  #σ²
-  epsilon::N  #ϵ
+  running_mean::W  # μ
+  running_var::W  #σ²
+  eps::N  #ϵ
   momentum::N
   active::Bool
 end
 
 BatchNorm(chs::Integer, activation_function = identity;
-          initβ = (i) -> zeros(Float32, i), initγ = (i) -> ones(Float32, i), ϵ = 1f-5, momentum = 0.1f0) =
-  BatchNorm(activation_function, param(initβ(chs)), param(initγ(chs)),
-            zeros(chs), ones(chs), epsilon, momentum, true)
+          initbias = (i) -> zeros(Float32, i), initscale = (i) -> ones(Float32, i), eps = 1f-5, momentum = 0.1f0) =
+  BatchNorm(activation_function, param(initbias(chs)), param(initscale(chs)),
+            zeros(chs), ones(chs), eps, momentum, true)
 
 function (BN::BatchNorm)(x)
   size(x, ndims(x)-1) == length(BN.bias) ||
@@ -121,34 +121,34 @@ function (BN::BatchNorm)(x)
   m = prod(size(x)[1:end-2]) * size(x)[end]
 
   if !BN.active
-    μ = reshape(BN.μ, affine_shape...)
-    σ² = reshape(BN.σ², affine_shape...)
+    running_mean = reshape(BN.running_mean, affine_shape...)
+    running_var = reshape(BN.running_var, affine_shape...)
   else
     T = eltype(x)
 
-    epsilon = data(convert(T, BN.epsilon))
+    epsilon = data(convert(T, BN.eps))
     axes = [1:dims-2; dims] # axes to reduce along (all but channels axis)
-    μ = mean(x, dims = axes)
-    σ² = sum((x .- μ) .^ 2, dims = axes) ./ m
+    running_mean = mean(x, dims = axes)
+    running_var = sum((x .- running_mean) .^ 2, dims = axes) ./ m
 
     # update moving mean/std
     mtm = data(convert(T, BN.momentum))
-    BN.μ = (1 - mtm) .* BN.μ .+ mtm .* reshape(data(μ), :)
-    BN.σ² = ((1 - mtm) .* BN.σ² .+ mtm .* reshape(data(σ²), :) .* m ./ (m - 1))
+    BN.running_mean = (1 - mtm) .* BN.running_mean .+ mtm .* reshape(data(running_mean), :)
+    BN.running_var = ((1 - mtm) .* BN.running_var .+ mtm .* reshape(data(running_var), :) .* m ./ (m - 1))
   end
 
   let activation_function = BN.activation_function
-    temp = reshape(scale, affine_shape...) .* ((x .- μ) ./ sqrt.(σ² .+ BN.epsilon)) 
+    temp = reshape(scale, affine_shape...) .* ((x .- running_mean) ./ sqrt.(running_var .+ BN.eps)) 
     # This is intentionally not fused because of an extreme slowdown doing so
     activation_function.(temp .+ reshape(bias, affine_shape...))
   end
 end
 
 children(BN::BatchNorm) =
-  (BN.activation_function, BN.bias, BN.scale, BN.μ, BN.σ², BN.epsilon, BN.momentum, BN.active)
+  (BN.activation_function, BN.bias, BN.scale, BN.running_mean, BN.running_var, BN.eps, BN.momentum, BN.active)
 
 mapchildren(f, BN::BatchNorm) =  # e.g. mapchildren(cu, BN)
-  BatchNorm(BN.activation_function, f(BN.bias), f(BN.scale), f(BN.μ), f(BN.σ²), BN.epsilon, BN.momentum, BN.active)
+  BatchNorm(BN.activation_function, f(BN.bias), f(BN.scale), f(BN.running_mean), f(BN.running_var), BN.eps, BN.momentum, BN.active)
 
 _testmode!(BN::BatchNorm, test) = (BN.active = !test)
 
