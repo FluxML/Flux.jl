8c467a71c0c1e6b2c4d903406b0e8c600c2103be
swiesend <sebastian@wiesendahl.de>
2019-03-06 23:54:30 +0100

    add an expressive text as training data

--- test/layers/recurrent.jl
+++ test/layers/recurrent.jl
@@ -3,14 +3,14 @@ using Flux
 using Flux: onehot, onehotbatch
 
 alphabet = map(c -> convert(Char, c), 0:255)
-data = onehotbatch("Hello, world!", alphabet)
+data = onehotbatch("This are forty-two onehotbatch characters.", alphabet)
 
 @testset "LSTM" begin
     m = Chain(
         LSTM(256,10),
         LSTM(10,256))
     
-    @test size(m(data)) == (256, 13)
+    @test size(m(data)) == (256, 42)
 end
 
 @testset "peephole LSTM" begin
@@ -18,7 +18,7 @@ end
         PLSTM(256,10),
         PLSTM(10,256))
 
-    @test size(m(data)) == (256, 13)
+    @test size(m(data)) == (256, 42)
 end
 
 @testset "fully connected LSTM" begin
@@ -26,5 +26,5 @@ end
         FCLSTM(256,10),
         FCLSTM(10,256))
 
-    @test size(m(data)) == (256, 13)
+    @test size(m(data)) == (256, 42)
 end
