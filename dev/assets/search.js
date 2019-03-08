f9d2f27fc9430f59f9852cdcfcda3379e1d1c0ef
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-08 11:50:46 +0000

    build based on b348e31

--- dev/assets/search.js
+++ dev/assets/search.js
@@ -39,8 +39,8 @@ parseUri.options = {
 requirejs.config({
     paths: {
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min',
-        'lunr': 'https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.1/lunr.min',
-        'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min',
+        'lunr': 'https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.5/lunr.min',
+        'lodash': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min',
     }
 });
 
