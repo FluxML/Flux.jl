47728b1899f0a87426473900e63419cc31c31e69
Johnny Chen <johnnychen94@hotmail.com>
2019-03-28 17:45:12 +0800

    fix test case error

--- test/layers/basic.jl
+++ test/layers/basic.jl
@@ -9,7 +9,7 @@ import Flux: activations
         @test_nowarn activations(dummy_model[1:end-1], x)
         @test_nowarn activations(dummy_model, x)
 
-        @test activations(Chain(), x)[1] == []
+        @test activations(Chain(), x) == []
         @test activations(dummy_model, x)[1] == dummy_model[1](x)
         @test activations(dummy_model, x)[2] == x |> dummy_model[1] |> dummy_model[2]
     end
