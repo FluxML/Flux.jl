63a9c22505051f719e2b867f35c94c9317dda187
Shreyas <shreyaskowshik@gmail.com>
2019-04-07 15:43:32 +0530

    Added iterator over epochs

--- src/data_iter.jl
+++ src/data_iter.jl
@@ -8,7 +8,7 @@ batches of data of size `batch_size`.
 
 Usage : 
 ```
-julia> x = rand(28,28,3,101)
+julia> x = rand(28,28,3,7)
 	   28×28×3×7 Array{Float64,4}: ...
 
 julia> bi = batch_iter(x,3) # An iterator over batches of data
@@ -37,4 +37,44 @@ batch_iter(c::AbstractArray,n::Int) = _batch_iter(c,n,nothing)
 function Base.iterate(b::BatchIter,state = 1)
     state <= length(ind) || return nothing
     return (b.c[[Colon() for _ in 1:ndims(b.c)-1]...,b.ind[state]],state+1)
+end
+
+"""
+Implemntation of iterators for epochs.
+Given a BatchIterator, `epoch_iter` will return an iterator over all batches 
+for the given number of epochs `ne`.
+
+Usage: 
+```
+julia> x = rand(28,28,3,7)
+       28×28×3×7 Array{Float64,4}: ...
+
+julia> bi = batch_iter(x,3) # An iterator over batches of data 
+
+julia> ei = epoch_iter(bi,3) # Iterator over batches for 3 epochs
+
+julia> first(ei)
+       28×28×3×3 Array{Float64,4}: ...
+```
+"""
+
+mutable struct EpochIter{T,N}
+    b::BatchIter{T,N}
+    ne::Int # Number of epochs
+end
+
+epoch_iter(b::BatchIter,ne::Int) = EpochIter(b,ne)
+
+function Base.iterate(e::EpochIter,state = 1)
+    state <= length(e.b.ind)*e.ne || return nothing
+    s = mod(state,length(e.b.ind))
+    if s == 0
+        s = length(e.b.ind)
+    end
+    return (e.b.c[[Colon() for _ in 1:ndims(e.b.c)-1]...,e.b.ind[s]],state+1)
+end
+
+function Base.length(e::EpochIter)
+    l = size(e.b.c)[end]
+    return (div(l, e.b.n) + ((mod(l, e.b.n) > 0) ? 1 : 0))*e.ne
 end
