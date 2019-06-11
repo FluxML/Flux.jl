f465665c735de3dc27e45fb40cf424e3eb70fcf8
thebhatman <manjunathbhat9920@gmail.com>
2019-06-11 20:20:00 +0530

    Corrected test for asymmetric padding

--- test/layers/conv.jl
+++ test/layers/conv.jl
@@ -25,8 +25,8 @@ end
 @testset "asymmetric padding" begin
   r = ones(Float32, 28, 28, 1, 1)
   m = Conv((3, 3), 1=>1, relu; pad=(0,1,1,2))
-  m.weight.data[:] .= 1.0
-  m.bias.data[:] .= 0.0
+  m.weight[:] .= 1.0
+  m.bias[:] .= 0.0
   y_hat = Flux.data(m(r))[:,:,1,1]
   @test size(y_hat) == (27, 29)
   @test y_hat[1, 1] ≈ 6.0
@@ -43,15 +43,15 @@ end
   @test size(m1(r), 3) == 15
   m2 = DepthwiseConv((2, 2), 3)
   @test size(m2(r), 3) == 3
-  
+
   x = zeros(Float64, 28, 28, 3, 5)
-  
+
   m3 = DepthwiseConv((2, 2), 3 => 5)
-  
+
   @test size(m3(r), 3) == 15
-  
+
   m4 = DepthwiseConv((2, 2), 3)
-  
+
   @test size(m4(r), 3) == 3
 end
 
