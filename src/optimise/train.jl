759fe9df2fb0a4665052383fae1b0fd8978a2f52
CarloLucibello <carlo.lucibello@gmail.com>
2020-02-26 20:27:39 +0100

    update docs and export update!

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -1,9 +1,22 @@
 using Juno
 import Zygote: Params, gradient
 
+
+"""
+  update!(opt, p, g)
+  update!(opt, ps::Params, gs)
+
+Perform an update step of the parameters `ps` (or the single parameter `p`) 
+according to optimizer `opt`  and the gradients `gs` (the gradient `g`).
+
+As a result, the parameters are mutated and the optimizer's internal state may change. 
+
+  update!(x, x̄)
+  
+Update the array `x` according to `x .-= x̄`.
+"""
 function update!(x::AbstractArray, x̄)
-  x .+= x̄
-  return x
+  x .-= x̄
 end
 
 function update!(opt, x, x̄)
