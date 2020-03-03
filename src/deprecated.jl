4516978caa77fe2407d05f25419c9102d233ef67
CarloLucibello <carlo.lucibello@gmail.com>
2020-03-03 18:25:46 +0100

    deprecate

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,5 +1,14 @@
 import Base: @deprecate
 
-
+#### remove in v 0.11   #####
 @deprecate param(x) x
 @deprecate data(x) x
+
+@deprecate mapleaves(f, x) fmap(f, x)
+
+macro treelike(args...)
+    functorm(args...)
+end
+#############################
+
+  
