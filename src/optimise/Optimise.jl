1dfec7f38bac2e0b0def9c6b4c4a2d2430784c33
Yao Lu <luyaocns@gmail.com>
2020-04-22 01:22:34 +0800

    add test

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -1,9 +1,12 @@
 module Optimise
 
+using LinearAlgebra
+
 export train!, update!,
 	SGD, Descent, ADAM, Momentum, Nesterov, RMSProp,
-	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,RADAM, 
-	InvDecay, ExpDecay, WeightDecay, stop, Optimiser
+	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW, RADAM, 
+	InvDecay, ExpDecay, WeightDecay, ClipValue, ClipNorm,
+	stop, Optimiser
 
 include("optimisers.jl")
 include("train.jl")
