c4f5e83697a2b6163c29c8ba2e45a394c00d9bdb
Yao Lu <luyaocns@gmail.com>
2020-04-22 01:24:13 +0800

    resolve conflict

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -5,8 +5,7 @@ using LinearAlgebra
 export train!, update!,
 	SGD, Descent, ADAM, Momentum, Nesterov, RMSProp,
 	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW, RADAM, 
-	InvDecay, ExpDecay, WeightDecay, ClipValue, ClipNorm,
-	stop, Optimiser
+	InvDecay, ExpDecay, WeightDecay, stop, Optimiser, ClipValue, ClipNorm
 
 include("optimisers.jl")
 include("train.jl")
