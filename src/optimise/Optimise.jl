007586858c4f0451f0be7b65149d497635024884
Yao Lu <luyaocns@gmail.com>
2020-05-14 17:13:35 +0800

    fix export merge conflict

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -2,10 +2,11 @@ module Optimise
 
 using LinearAlgebra
 
-export train!, update!, stop, Optimiser,
-		Descent, ADAM, Momentum, Nesterov, RMSProp,
-		ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW, RADAM,
-		InvDecay, ExpDecay, WeightDecay, ClipValue, ClipNorm
+export train!, update!,
+	Descent, ADAM, Momentum, Nesterov, RMSProp,
+	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,RADAM, 
+	InvDecay, ExpDecay, WeightDecay, stop, Optimiser,
+	ClipValue, ClipNorm
 
 include("optimisers.jl")
 include("train.jl")
