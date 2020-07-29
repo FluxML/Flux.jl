86fb5cde16823107686419bcbcd8b560682697fe
Matěj Račinský <matej.racinsky@avast.com>
2020-07-29 17:12:20 +0200

    optimization of normalisation by passing mean

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -19,7 +19,6 @@ Normalise `x` to mean 0 and standard deviation 1 across the dimensions given by 
 """
 function normalise(x::AbstractArray; dims, ϵ=ofeltype(x, 1e-5))
   μ = mean(x, dims=dims)
-    #   σ = std(x, dims=dims, mean=μ, corrected=false) # use this when #478 gets merged
-  σ = std(x, dims=dims, corrected=false)
+  σ = std(x, dims=dims, mean=μ, corrected=false)
   return (x .- μ) ./ (σ .+ ϵ)
 end
