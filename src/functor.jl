63d3924ed532877b45e7b3dfaca436e511d64d23
DrChainsaw <Christian.kyril.skarby@gmail.com>
2020-10-30 21:53:52 +0100

    Change adaot to adapt_structure for Zeros

--- src/functor.jl
+++ src/functor.jl
@@ -1,4 +1,4 @@
-import Adapt: adapt, adapt_storage
+import Adapt: adapt, adapt_storage, adapt_structure
 using  LinearAlgebra: Cholesky
 using Zygote: IdSet
 import Functors: @functor, functor, fmap
@@ -78,7 +78,7 @@ gpu(x) = use_cuda[] ? fmap(CUDA.cu, x) : x
 adapt_storage(T::Type{<:Real}, xs::AbstractArray{<:Real}) = convert.(T, xs)
 
 paramtype(T::Type{<:Real}, m) = fmap(x -> adapt(T, x), m)
-adapt(::Any, x::Zeros) = x # So that we for example don't accidentally end up with layers having a scalar for bias
+adapt_structure(to, x::Flux.Zeros) = x # So that we for example don't accidentally end up with layers having a scalar for bias
 
 f32(m) = paramtype(Float32, m)
 f64(m) = paramtype(Float64, m)
