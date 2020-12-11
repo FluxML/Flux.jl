045f1e9b67956dec419c95fee901717983eced0d
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2020-12-11 13:08:49 +0530

    remove `@jit` macro

--- src/utils.jl
+++ src/utils.jl
@@ -471,18 +471,3 @@ function throttle(f, timeout; leading=true, trailing=false)
     return result
   end
 end
-
-"""
-    @jit ...
-
-The `@jit` annotation can be applied to any code, and the code will be compiled
-for performance.
-
-    @jit f(x) = @jit(x) + @jit(x)
-
-Note that compilation happens regardless of the `@jit` macro, so it should only
-be used for aesthetic purposes, or by recovering Python users.
-"""
-macro jit(ex)
-  esc(ex)
-end
