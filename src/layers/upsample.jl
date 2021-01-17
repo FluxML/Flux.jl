39b892de81484a24276140ab5cfe021cb700019e
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2021-01-17 07:54:43 +0100

    Update src/layers/upsample.jl
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- src/layers/upsample.jl
+++ src/layers/upsample.jl
@@ -21,17 +21,24 @@ julia> m(ones(1,1,1,1))
  1.0  1.0  1.0
 ```
 """
-struct Upsample{Mode, N, T} 
-  scale::NTuple{N, T}
+struct Upsample{Mode,T}
+  scale::T
 end
 
-function Upsample(; scale::NTuple{N, T}, mode::Symbol) where {N,T}
-  @assert mode in [:bilinear] "Unsupported mode '$mode'"
-  @assert N == 2  "Only 2d upsampling currently supported"
-  Upsample{Val{mode}, N, T}(scale)
+function Upsample(; scale, mode::Symbol=:bilinear)
+  mode in [:bilinear] || throw(ArgumentError("only bilinear sampling is supported"))
+  scale isa Integer || scale isa NTuple{2,<:Integer} || 
+    throw(ArgumentError("only two-dimensional scaling by an integer factor is supported"))
+  Upsample{mode, typeof(scale)}(scale)
 end
 
-(m::Upsample{Val{:bilinear}})(x) = NNlib.upsample_bilinear(x, m.scale)
+(m::Upsample{:bilinear})(x) = NNlib.upsample_bilinear(x, m.scale isa Tuple ? m.scale : (m.scale, m.scale))
+
+function Base.show(io::IO, u::Upsample2{mode}) where {mode}
+  print(io, "Upsample(scale=", u.scale)
+  mode == :bilinear || print(io, ", mode=", mode)
+  print(io, ")")
+end
 
 
 """
