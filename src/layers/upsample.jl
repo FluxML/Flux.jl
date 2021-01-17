96ab813158baf3656c24c24b1a12a9e482a3eda1
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-01-17 08:15:32 +0100

    small fixes

--- src/layers/upsample.jl
+++ src/layers/upsample.jl
@@ -1,8 +1,8 @@
 """
     Upsample(; scale, mode)
 
-An upsampling layer. `scale` is a tuple determining 
-the output rescaling factor along each spatial dimension.
+An upsampling layer. `scale` is an integer or a tuple of integerers 
+representing  the output rescaling factor along each spatial dimension.
 The only currently supported upsampling `mode` is 
 `:bilinear`.
 
@@ -12,7 +12,7 @@ See [`NNlib.upsample_bilinear`](@ref).
 
 ```juliarepl
 julia> m = Upsample(scale=(2,3), mode=:bilinear)
-Upsample{Val{:bilinear},2,Int64}((2, 3))
+Upsample(scale=(2, 3), mode=bilinear)
 
 julia> m(ones(1,1,1,1))
 2×3×1×1 Array{Float64,4}:
@@ -25,18 +25,19 @@ struct Upsample{Mode,T}
   scale::T
 end
 
-function Upsample(; scale, mode::Symbol=:bilinear)
-  mode in [:bilinear] || throw(ArgumentError("only bilinear sampling is supported"))
-  scale isa Integer || scale isa NTuple{2,<:Integer} || 
-    throw(ArgumentError("only two-dimensional scaling by an integer factor is supported"))
-  Upsample{mode, typeof(scale)}(scale)
+function Upsample(; scale::Union{Int, NTuple{N,Int}}, mode::Symbol) where N
+  mode in [:linear, :bilinear] || 
+    throw(ArgumentError("only sampling is currently supported"))
+  scale isa Int  || N == 2 ||
+    throw(ArgumentError("only two-dimensional scaling  is supported"))
+  return Upsample{mode, typeof(scale)}(scale)
 end
 
 (m::Upsample{:bilinear})(x) = NNlib.upsample_bilinear(x, m.scale isa Tuple ? m.scale : (m.scale, m.scale))
 
-function Base.show(io::IO, u::Upsample2{mode}) where {mode}
+function Base.show(io::IO, u::Upsample{mode}) where {mode}
   print(io, "Upsample(scale=", u.scale)
-  mode == :bilinear || print(io, ", mode=", mode)
+  print(io, ", mode=:", mode)
   print(io, ")")
 end
 
