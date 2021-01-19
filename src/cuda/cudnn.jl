89682d68d2c1b8a1147477fd00d95becd90cbaa1
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-01-19 15:00:10 +0530

    rm asserts
    Asserts are also disabled at certain times, so it makes sense to retain runtime checks
    
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- src/cuda/cudnn.jl
+++ src/cuda/cudnn.jl
@@ -3,9 +3,9 @@ import CUDA.CUDNN: batchnorm, ∇batchnorm
 function (BN::Flux.BatchNorm)(x::Union{CuArray{T,2},CuArray{T,4},CuArray{T,5}},
                               cache=nothing) where T<:Union{Float32, Float64}
   
-  @assert BN.affine "BatchNorm: only affine=true supported on gpu"
-  @assert BN.track_stats "BatchNorm: only track_stats=true supported on gpu"
-  @assert length(BN.β) == size(x, ndims(x)-1) "BatchNorm: input has wronng number of channels"
+  BN.affine || throw(ArgumentError("BatchNorm: only affine=true supported on gpu"))
+  BN.track_stats || throw(ArgumentError("BatchNorm: only track_stats=true supported on gpu"))
+  length(BN.β) == size(x, ndims(x)-1) || throw(ArgumentError("BatchNorm: input has wronng number of channels"))
   return BN.λ.(batchnorm(BN.γ, BN.β, x, BN.μ, BN.σ², BN.momentum; 
                   cache=cache, alpha=1, beta=0, eps=BN.ϵ, 
                   training=Flux._isactive(BN)))
