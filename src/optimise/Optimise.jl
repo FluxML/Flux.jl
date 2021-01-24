de65314729ed7de3e356418e0ede39a2969eac05
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-01-25 01:35:17 +0530

    add Optimisers

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -1,9 +1,10 @@
 module Optimise
 
 using LinearAlgebra
+using Optimisers
+using Optimisers: Descent, ADAM, Momentum, Nesterov, RMSProp
 
 export train!, update!,
-	Descent, ADAM, Momentum, Nesterov, RMSProp,
 	ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,RADAM, OADAM, AdaBelief,
 	InvDecay, ExpDecay, WeightDecay, stop, skip, Optimiser,
 	ClipValue, ClipNorm
