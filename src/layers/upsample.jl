93287ddd8655ff8b88aded53f3d2962b4e70720f
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-02-05 09:36:26 +0100

    update

--- src/layers/upsample.jl
+++ src/layers/upsample.jl
@@ -1,20 +1,25 @@
 """
   Upsample(scale; mode=:nearest)  
-  Upsample(; scale, mode=:nearest)
+  Upsample(; size, mode=:bilinear)
 
 An upsampling layer. `scale` is a number or a tuple of numbers 
 representing  the output rescaling factor along each spatial dimension.
+
 Currently supported upsampling `mode`s are:
   - `:nearest`
   - `:bilinear`
+
+For some `mode`s it is possible
+to directly specify the output spatial `size`,
+as an alternative to `scale`,   
 
 See [`NNlib.upsample_nearest`](@ref), [`NNlib.upsample_bilinear`](@ref).
 
 # Examples
 
 ```juliarepl
-julia> m = Upsample(scale=(2,3), mode=:bilinear)
-Upsample(scale=(2, 3), mode=bilinear)
+julia> m = Upsample((2,3), mode=:bilinear)
+Upsample((2, 3), mode=bilinear)
 
 julia> m(ones(1,1,1,1))
 2×3×1×1 Array{Float64,4}:
@@ -23,34 +28,38 @@ julia> m(ones(1,1,1,1))
  1.0  1.0  1.0
 ```
 """
-struct Upsample{Mode,T}
-  scale::T
+struct Upsample{Mode,S,T}
+  scale::S
+  size::T
 end
-
-Upsample(; scale, mode::Symbol=:nearest) = Upsample(scale; mode)
 
 function Upsample(scale; mode::Symbol=:nearest)
   mode in [:nearest, :bilinear] || 
     throw(ArgumentError("mode=:$mode is not supported."))
-  return Upsample{mode, typeof(scale)}(scale)
+  return Upsample(scale, nothing)
 end
 
-(m::Upsample{:nearest,<:Number})(x::AbstractArray{T,N}) where {T,N} =
-  NNlib.upsample_nearest(x, ntuple(_ -> m.scale, N-2))
-(m::Upsample{:nearest,<:Tuple})(x::AbstractArray) =
+function Upsample(; size, mode::Symbol=:bilinear)
+  mode in [:nearest, :bilinear] || 
+    throw(ArgumentError("mode=:$mode is not supported."))
+  return Upsample(nothing, size)
+end
+
+(m::Upsample{:nearest})(x::AbstractArray) =
   NNlib.upsample_nearest(x, m.scale)
 
-(m::Upsample{:bilinear,<:Number})(x::AbstractArray{T,N}) where {T,N} =
-  NNlib.upsample_bilinear(x, ntuple(_ -> m.scale, N-2))
-(m::Upsample{:bilinear,<:Tuple})(x::AbstractArray) =
+(m::Upsample{:bilinear})(x::AbstractArray) =
   NNlib.upsample_bilinear(x, m.scale)
+(m::Upsample{:bilinear, Nothing})(x::AbstractArray) = 
+  NNlib.upsample_bilinear(x; size=m.size)
 
 function Base.show(io::IO, u::Upsample{mode}) where {mode}
-  print(io, "Upsample(scale=", u.scale)
-  print(io, ", mode=:", mode)
-  print(io, ")")
+  print(io, "Upsample(")
+  u.scale !== nothing && print(io, "$(u.scale), ")
+  u.size !== nothing && print(io, "size=$(u.size), ")
+  print(io, "mode=:", mode)
+  println(io, ")")
 end
-
 
 """
     PixelShuffle(r::Int)
@@ -64,3 +73,5 @@ struct PixelShuffle
 end
 
 (m::PixelShuffle)(x) = NNlib.pixel_shuffle(x, m.r)
+
+
