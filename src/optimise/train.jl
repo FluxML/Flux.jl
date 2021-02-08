d4f27a719a7b8a9de148950ca3a1d4a582da39f1
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-02-08 21:29:40 +0530

    rm train changes

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -1,50 +1,43 @@
 using Juno
-import Zygote: Params, pullback, gradient, sensitivity
+import Zygote: Params, gradient
 
-# """
-#     update!(x, x̄)
-# 
-# Update the array `x` according to `x .-= x̄`.
-# """
-# function update!(x::AbstractArray, x̄)
-#   x .-= x̄
-# end
-# 
-# """
-#     update!(opt, p, g)
-#     update!(opt, ps::Params, gs)
-# 
-# Perform an update step of the parameters `ps` (or the single parameter `p`)
-# according to optimizer `opt`  and the gradients `gs` (the gradient `g`).
-# 
-# As a result, the parameters are mutated and the optimizer's internal state may change.
-# """
-# function update!(opt, x, x̄, st)
-#   Δ, st = x .- apply(opt, x, x̄, st)
-#   update!(x, Δ)
-#   Δ, st
-# end
-# 
-# function update!(opt, xs::Params, gs, st)
-#   for x in xs
-#     gs[x] == nothing && continue
-#     _, st = update!(opt, x, gs[x], st)
-#   end
-# end
+"""
+    update!(x, x̄)
+Update the array `x` according to `x .-= x̄`.
+"""
+function update!(x::AbstractArray, x̄)
+  x .-= x̄
+end
+
+"""
+    update!(opt, p, g)
+    update!(opt, ps::Params, gs)
+Perform an update step of the parameters `ps` (or the single parameter `p`)
+according to optimizer `opt`  and the gradients `gs` (the gradient `g`).
+As a result, the parameters are mutated and the optimizer's internal state may change.
+"""
+function update!(opt, x, x̄)
+  x .-= apply!(opt, x, x̄)
+end
+
+function update!(opt, xs::Params, gs)
+  for x in xs
+    gs[x] == nothing && continue
+    update!(opt, x, gs[x])
+  end
+end
 
 # Callback niceties
 call(f, xs...) = f(xs...)
 runall(f) = f
-runall(fs::AbstractVector) = (x...) -> foreach(f -> call(f, x...), fs)
+runall(fs::AbstractVector) = () -> foreach(call, fs)
 
 struct SkipException <: Exception end
 
 """
     skip()
-
 Call `Flux.skip()` in a callback to indicate when a callback condition is met.
 This will trigger the train loop to skip the current data point and not update with the calculated gradient.
-
 # Examples
 ```julia
 cb = function ()
@@ -61,10 +54,8 @@ struct StopException <: Exception end
 
 """
     stop()
-
 Call `Flux.stop()` in a callback to indicate when a callback condition is met.
 This will trigger the train loop to stop and exit.
-
 # Examples
 ```julia
 cb = function ()
@@ -81,69 +72,25 @@ batchmemaybe(x::Tuple) = x
 
 """
     train!(loss, params, data, opt; cb)
-
 For each datapoint `d` in `data`, compute the gradient of  `loss` with
 respect to `params` through backpropagation and call the optimizer `opt`.
-
 If `d` is a tuple of arguments to `loss` call `loss(d...)`, else call `loss(d)`.
-
 A callback is given with the keyword argument `cb`. For example, this will print
 "training" every 10 seconds (using [`Flux.throttle`](@ref)):
-
     train!(loss, params, data, opt, cb = throttle(() -> println("training"), 10))
-
 The callback can call [`Flux.stop`](@ref) to interrupt the training loop.
-
 Multiple optimisers and callbacks can be passed to `opt` and `cb` as arrays.
 """
-# function train!(loss, ps, data, opt; cb = () -> ())
-#   ps = Params(ps)
-#   cb = runall(cb)
-#   st = Optimisers.init(opt, ps)
-#   @progress for d in data
-#     try
-#       gs = gradient(ps) do
-#         loss(batchmemaybe(d)...)
-#       end
-#       update!(opt, ps, gs)
-#       cb()
-#     catch ex
-#       if ex isa StopException
-#         break
-#       elseif ex isa SkipException
-#         continue
-#       else
-#         rethrow(ex)
-#       end
-#     end
-#   end
-# end
-
-function train(m, loss, data, opt; cb = (x...) -> (),
-                                    prehook = (x...) -> (),
-                                    posthook = (x...) -> ())
-  st = [Optimisers.init(opt, p) for p in Flux.params(m)]
-  prehook = runall(prehook)
-  posthook = runall(posthook)
+function train!(loss, ps, data, opt; cb = () -> ())
+  ps = Params(ps)
   cb = runall(cb)
-
-  dlen = try
-    length(data)
-  catch e
-    @warn "Dataset length unkown"
-    0
-  end
-
-  for d in data
+  @progress for d in data
     try
-      ŷ, back = pullback(m) do m
-        loss(m, batchmemaybe(d)...)
+      gs = gradient(ps) do
+        loss(batchmemaybe(d)...)
       end
-      prehook(ŷ)
-      m̂, = back(sensitivity(ŷ))
-      posthook(ŷ, m, m̂)
-      m, st = opt(m, m̂, st)
-      cb(ŷ, m, m̂)
+      update!(opt, ps, gs)
+      cb()
     catch ex
       if ex isa StopException
         break
@@ -154,16 +101,12 @@ function train(m, loss, data, opt; cb = (x...) -> (),
       end
     end
   end
-
-  m
 end
 
 """
     @epochs N body
-
 Run `body` `N` times. Mainly useful for quickly doing multiple epochs of
 training in a REPL.
-
 # Examples
 ```jldoctest
 julia> Flux.@epochs 2 println("hello")
@@ -179,11 +122,3 @@ macro epochs(n, ex)
       $(esc(ex))
     end)
 end
-
-# macro epochs(n, f, ex)
-#   :(@progress for i = 1:$(esc(n))
-#       @info "Epoch $i"
-#       $(esc(ex))
-#       $(esc(f($i)))
-#     end)
-# end
