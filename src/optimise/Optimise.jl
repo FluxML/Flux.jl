06c659966f5880daf1a68532b14ff959164fd805
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-02-09 20:40:58 +0530

    fix exports

--- src/optimise/Optimise.jl
+++ src/optimise/Optimise.jl
@@ -3,10 +3,10 @@ module Optimise
 using LinearAlgebra
 using Optimisers
 
-export train!, 
+export train!, skip, stop
+       Descent, ADAM, Momentum, Nesterov, RMSProp, 
        ADAGrad, AdaMax, ADADelta, AMSGrad, NADAM, ADAMW,RADAM, OADAM, AdaBelief,
-       InvDecay, ExpDecay, WeightDecay, stop, skip, Optimiser,
-       ClipValue, ClipNorm
+       InvDecay, ExpDecay, WeightDecay, stop, skip, ChainOptimiser
 
 include("train.jl")
 
