05a9a2d810a86dbf16916dd66583d0e9eb74cd8b
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-02-10 23:36:29 +0100

    add other constructor + small changes

--- src/layers/upsample.jl
+++ src/layers/upsample.jl
@@ -1,5 +1,6 @@
 """
-  Upsample(mode = :nearest; [scale, size])  
+  Upsample(mode = :nearest; [scale, size]) 
+  Upsample(scale, mode = :nearest)  
 
 An upsampling layer. One of two keywords must be given:
 
@@ -16,30 +17,32 @@ and corresponding NNlib's methods are:
 
 ```juliarepl
 julia> m = Upsample(scale = (2, 3))
-Upsample(:nearest, scale=(2, 3))
+Upsample(:nearest, scale = (2, 3))
 
 julia> m(ones(2, 2, 1, 1)) |> size
 (4, 6, 1, 1)
 
 julia> m = Upsample(:bilinear, size = (4, 5))
-Upsample(:bilinear, size=(4, 5))
+Upsample(:bilinear, size = (4, 5))
 
 julia> m(ones(2, 2, 1, 1)) |> size
 (4, 5, 1, 1)
 """
-struct Upsample{Mode,S,T}
+struct Upsample{mode, S, T}
   scale::S
   size::T
 end
 
-function Upsample(mode = :nearest; scale = nothing, size = nothing)
+function Upsample(mode::Symbol = :nearest; scale = nothing, size = nothing)
   mode in [:nearest, :bilinear] || 
     throw(ArgumentError("mode=:$mode is not supported."))
-  if ~((scale === nothing) ⊻ (size === nothing))
-    throw(ArgumentError("Either scale or size should be specified."))
+  if !(isnothing(scale) ⊻ isnothing(size))
+      throw(ArgumentError("Either scale or size should be specified (but not both)."))
   end
   return Upsample{mode,typeof(scale),typeof(size)}(scale, size)
 end
+
+Upsample(scale, mode::Symbol = :nearest) = Upsample(mode; scale)
 
 (m::Upsample{:nearest})(x::AbstractArray) =
   NNlib.upsample_nearest(x, m.scale)
