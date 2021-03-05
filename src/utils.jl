8800db8981000b13011230a18619b48e98fc8252
DrChainsaw <36208552+DrChainsaw@users.noreply.github.com>
2021-03-05 09:10:15 +0100

    Add explicit eltype in identity_init
    Co-authored-by: Kyle Daruwalla <daruwalla.k.public@icloud.com>

--- src/utils.jl
+++ src/utils.jl
@@ -354,7 +354,7 @@ identity_init(rows, cols; gain=1) = Matrix{Float32}(I * gain, rows,cols)
 function identity_init(dims...; gain=1)
   nin, nout = dims[end-1], dims[end]
   centers = map(d -> cld(d, 2), dims[1:end-2])
-  weights = zeros(dims)
+  weights = zeros(Float32, dims)
   for i in 1:min(nin,nout)
     weights[centers..., i, i] = gain
   end
