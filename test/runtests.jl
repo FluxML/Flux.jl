1b5cedbd002136e2d07248c6851940b1fca422a1
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-08 18:29:32 +0530

    revert tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,53 +6,53 @@ using IterTools: ncycle
 
 Random.seed!(0)
 
-# @testset "Utils" begin
-#   include("utils.jl")
-# end
-# 
-# @testset "Onehot" begin
-#   include("onehot.jl")
-# end
-# 
-# @testset "Optimise" begin
-#   include("optimise.jl")
-# end
+@testset "Utils" begin
+  include("utils.jl")
+end
+
+@testset "Onehot" begin
+  include("onehot.jl")
+end
+
+@testset "Optimise" begin
+  include("optimise.jl")
+end
 
 @testset "Data" begin
   include("data.jl")
 end
 
-# @testset "Losses" begin
-#   include("losses.jl")
-#   include("ctc.jl")
-#   if Flux.use_cuda[] include("ctc-gpu.jl") end
-# end
-# 
-# @testset "Layers" begin
-#   include("layers/basic.jl")
-#   include("layers/normalisation.jl")
-#   include("layers/stateless.jl")
-#   include("layers/recurrent.jl")
-#   include("layers/conv.jl")
-# end
-# 
-# @testset "outputsize" begin
-#   using Flux: outputsize
-#   include("outputsize.jl")
-# end
-# 
-# @testset "CUDA" begin
-#   if Flux.use_cuda[]
-#     include("cuda/runtests.jl")
-#   else
-#     @warn "CUDA unavailable, not testing GPU support"
-#   end
-# end
-# 
-# @static if VERSION == v"1.5"
-#   using Documenter
-#   @testset "Docs" begin
-#     DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive=true)
-#     doctest(Flux)
-#   end
-# end
+@testset "Losses" begin
+  include("losses.jl")
+  include("ctc.jl")
+  if Flux.use_cuda[] include("ctc-gpu.jl") end
+end
+
+@testset "Layers" begin
+  include("layers/basic.jl")
+  include("layers/normalisation.jl")
+  include("layers/stateless.jl")
+  include("layers/recurrent.jl")
+  include("layers/conv.jl")
+end
+
+@testset "outputsize" begin
+  using Flux: outputsize
+  include("outputsize.jl")
+end
+
+@testset "CUDA" begin
+  if Flux.use_cuda[]
+    include("cuda/runtests.jl")
+  else
+    @warn "CUDA unavailable, not testing GPU support"
+  end
+end
+
+@static if VERSION == v"1.5"
+  using Documenter
+  @testset "Docs" begin
+    DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive=true)
+    doctest(Flux)
+  end
+end
