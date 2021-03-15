b281a427ec435f0c1b22aae11e4beaff3b936f5d
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-15 15:05:50 +0530

    rm CUDA CUDNN; use NNlibCUDA

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -1,11 +1,12 @@
 module CUDAint
 
 using ..CUDA
-using CUDA: CUDNN
+# using CUDA: CUDNN
 
 import ..Flux: Flux
 import Zygote
 using Zygote: @adjoint
+import NNlib, NNlibCUDA
 
 include("cudnn.jl")
 
