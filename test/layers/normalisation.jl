db69b3aac1e5dc3154c69c8028d297adb742845e
ShoofLLC <15924772+ShoofLLC@users.noreply.github.com>
2021-04-10 15:29:38 +0200

    Update normalisation.jl

--- test/layers/normalisation.jl
+++ test/layers/normalisation.jl
@@ -4,7 +4,7 @@ using Zygote: pullback
 evalwgrad(f, x...) = pullback(f, x...)[1]
 
 @testset "Dropout" begin
-  x = [1.+0im,2.+1im,3.+3im]
+  x = [1.0+0im,2.0+1im,3.0+3im]
   @test x == Dropout(0.1)(x)
   @test x == evalwgrad(Dropout(0), x)
   @test zero(x) == evalwgrad(Dropout(1), x)
