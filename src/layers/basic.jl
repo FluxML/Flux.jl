fec920b7439a1421bf0ec99ed39872f3ad3557f9
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-08-04 11:42:12 -0400

    Update src/layers/basic.jl
    Co-authored-by: Dhairya Gandhi <dhairya@juliacomputing.com>

--- src/layers/basic.jl
+++ src/layers/basic.jl
@@ -83,7 +83,7 @@ extraChain(::Tuple{}, x) = ()
     Dense(in, out, σ=identity; bias=true, init=glorot_uniform)
     Dense(W::AbstractMatrix, [bias, σ])
 
-Create a traditional fully-connected layer, whose forward pass is given by:
+Create a traditional `Dense` layer, whose forward pass is given by:
 
     y = σ.(W * x .+ bias)
 
