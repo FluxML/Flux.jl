bf2e87684ef6507a8afb06a3cc05fafc81ba0a47
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-08-24 11:56:40 +0530

    typo

--- test/layers/basic.jl
+++ test/layers/basic.jl
@@ -258,8 +258,7 @@ import Flux: activations
 
       (m::MIMO)(x::Tuple) = m(x...)
       mimo_output1 = p(x, x)
-      mimo_ouput2 = p(x) # to check for N layers 1 input case
-      @test mimo_output1 ≈ mimo_output2
+      mimo_output2 = p(x) # to check for N layers 1 input case
       @test length(mimo_output1) == 2
       @test all(x -> length(x) == 3, mimo_output1)
     end
