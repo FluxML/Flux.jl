6e2da25beb811161ed67c20b948fe4db2d65d5f7
Matěj Račinský <matej.racinsky@avast.com>
2021-10-26 18:50:09 +0200

    returning to default implementation for onehot because I don't want to break it.

--- src/onehot.jl
+++ src/onehot.jl
@@ -220,7 +220,7 @@ end
 
 @nograd OneHotArray, onecold, onehot, onehotbatch
 
-function Base.:(*)(A::AbstractMatrix, B::OneHotLike{<:Any, L, 0}) where L
+function Base.:(*)(A::AbstractMatrix, B::OneHotLike{<:Any, L}) where L
   _isonehot(B) || return invoke(*, Tuple{AbstractMatrix, AbstractMatrix}, A, B)
   size(A, 2) == L || throw(DimensionMismatch("Matrix column must correspond with OneHot size: $(size(A, 2)) != $L"))
   return A[:, onecold(B)]
