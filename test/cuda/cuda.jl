931d4093f76f5422dd274b437b0c5ae7c405ba7e
Matěj Račinský <racinsky.matej@seznam.cz>
2021-10-26 11:13:28 +0200

    Update test/cuda/cuda.jl
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- test/cuda/cuda.jl
+++ test/cuda/cuda.jl
@@ -43,7 +43,7 @@ end
   y = Flux.onehotbatch(ones(3), 1:2) |> gpu;
   @test (repr("text/plain", y); true)
 
-  gA = rand(3, 3) |> gpu;
+  gA = rand(3, 2) |> gpu;
   @test gradient(A -> sum(A * y), gA)[1] isa CuArray
 end
 
