1525b303339c6f6a4ed1fb4a4280660d5f10e84b
Matthew Schlegel <mkschleg@gmail.com>
2022-02-13 13:30:40 -0700

    Make lower n to 1k instead of 2k to fit in 8GB. Try-catch for gpumem

--- perf/recurrent.jl
+++ perf/recurrent.jl
@@ -1,22 +1,40 @@
 
 println("RNN")
-for n in [2, 20, 200, 2000], T in [1, 8, 16, 64]
+for n in [2, 20, 200, 1000], T in [1, 8, 16, 64]
   x = [randn(Float32, n, n) for t in 1:T]
   model = RNN(n, n)
   println("CPU n=$n, t=$T")
   run_benchmark(model, x, cuda=false)
   println("CUDA n=$n, t=$T")
-  run_benchmark(model, x, cuda=true)    
+  try
+      run_benchmark(model, x, cuda=true)
+  catch ex
+      @show typeof(ex)
+      if ex isa OutOfGPUMemoryError
+          @warn "Not enough GPU memory to run test"
+      else
+          rethrow(ex)
+      end
+  end
 end
 
 println("RNN-3d")
-for n in [2, 20, 200, 2000], T in [1, 8, 16, 64]
+for n in [2, 20, 200, 1000], T in [1, 8, 16, 64]
   x = randn(Float32, n, n, T)
   model = RNN(n, n)
   println("CPU n=$n, t=$T")
   run_benchmark(model, x, cuda=false)
   println("CUDA n=$n, t=$T")
-  run_benchmark(model, x, cuda=true)    
+  try
+      run_benchmark(model, x, cuda=true)
+  catch ex
+      @show typeof(ex)
+      if ex isa OutOfGPUMemoryError
+          @warn "Not enough GPU memory to run test"
+      else
+          rethrow(ex)
+      end
+  end
 end
 
 
