dc029af0be970ccb556aec4e99ae8eb8cf65fd2e
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-02-21 00:31:21 +0530

    Update functions.jl

--- src/losses/functions.jl
+++ src/losses/functions.jl
@@ -561,7 +561,7 @@ true
 See also: [`Losses.binary_focal_loss`](@ref) for binary (not one-hot) labels
                                     
 """
-function contrastive_loss(ŷ, y; agg = mean, margin = epseltype(ŷ))
+function contrastive_loss(ŷ, y,margin = epseltype(ŷ); agg=mean)
     max_val = max(sum(ŷᵢ -> (margin - ŷᵢ), ŷ), 0)
     max_val > 0 ? margin_square = (margin .- ŷ) .^ 2 : margin_square = 0
     return agg(y .* (ŷ .^ 2) + (1 .- y) .* margin_square)
