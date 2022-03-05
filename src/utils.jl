d559685a369ff28c008041c744351ce0ee4aab85
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-03-05 08:17:39 -0500

    Update src/utils.jl
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- src/utils.jl
+++ src/utils.jl
@@ -447,7 +447,7 @@ Return a bias parameter for a layer, based on the value given
 to the constructor's keyword `bias=bias`.
 
 * `bias == true` creates a trainable array of the given size, of the same type as `weights`, initialised to zero.
-* `bias == false` returns `false` now, which is understood by AD to be non-differentiable.
+* `bias == false` returns `false`, which is understood by AD to be non-differentiable.
 * `bias::AbstractArray` uses the array provided, provided it has the correct size.
   It does not at present correct the `eltype` to match that of `weights`.
 """
