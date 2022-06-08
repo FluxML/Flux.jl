9d48e511a303f0cf905785d3f7c8f6d3621f0e4e
Saransh <saransh0701@gmail.com>
2022-06-08 12:07:08 +0530

    Resolve conflicts (rebase error)

--- docs/make.jl
+++ docs/make.jl
@@ -1,41 +1,5 @@
 using Documenter, Flux, NNlib, Functors, MLUtils, BSON
 
-<<<<<<< HEAD
-=======
-DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive = true)
-makedocs(modules = [Flux, NNlib, Functors, MLUtils, BSON],
-         doctest = false,
-         sitename = "Flux",
-         pages = ["Home" => "index.md",
-                  "Building Models" =>
-                    ["Overview" => "models/overview.md",
-                     "Basics" => "models/basics.md",
-                     "Recurrence" => "models/recurrence.md",
-                     "Model Reference" => "models/layers.md",
-                     "Loss Functions" => "models/losses.md",
-                     "Regularisation" => "models/regularisation.md",
-                     "Advanced Model Building" => "models/advanced.md",
-                     "NNlib" => "models/nnlib.md",
-                     "Functors" => "models/functors.md"],
-                  "Handling Data" =>
-                    ["One-Hot Encoding" => "data/onehot.md",
-                     "MLUtils" => "data/mlutils.md"],
-                  "Training Models" =>
-                    ["Optimisers" => "training/optimisers.md",
-                     "Training" => "training/training.md"],
-                  "GPU Support" => "gpu.md",
-                  "Saving & Loading" => "saving.md",
-                  "The Julia Ecosystem" => "ecosystem.md",
-                  "Utility Functions" => "utilities.md",
-                  "Performance Tips" => "performance.md",
-                  "Datasets" => "datasets.md",
-                  "Community" => "community.md"],
-         format = Documenter.HTML(
-             analytics = "UA-36890222-9",
-             assets = ["assets/flux.css"],
-             prettyurls = get(ENV, "CI", nothing) == "true"),
-         )
->>>>>>> 8f7dc934 (Add and fix some docstrings)
 
 DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive = true)
 
