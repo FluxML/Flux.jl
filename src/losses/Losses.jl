a7f584914085a0430eb9f588bda80abde0e7fbcb
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-07-26 15:37:34 -0400

    Remove CTC loss (moved to NNlib) (#2024)
    * remove CTC loss, moved to NNlib
    
    * trigger
    
    * don't export ctc_loss

--- src/losses/Losses.jl
+++ src/losses/Losses.jl
@@ -6,7 +6,7 @@ using Zygote: @adjoint
 using ChainRulesCore
 using ..Flux: ofeltype, epseltype
 using CUDA
-using NNlib: logsoftmax, logσ
+using NNlib: logsoftmax, logσ, ctc_loss, ctc_alpha, ∇ctc_loss
 import Base.Broadcast: broadcasted
 
 export mse, mae, msle,
@@ -19,11 +19,9 @@ export mse, mae, msle,
     dice_coeff_loss,
     poisson_loss,
     hinge_loss, squared_hinge_loss,
-    ctc_loss,
     binary_focal_loss, focal_loss, siamese_contrastive_loss
 
 include("utils.jl")
 include("functions.jl")
-include("ctc.jl")
 
 end #module
