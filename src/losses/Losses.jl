891709920a50148e038658d71a9b8b0129959517
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-10-18 13:03:33 -0400

    rm module docstring

--- src/losses/Losses.jl
+++ src/losses/Losses.jl
@@ -1,28 +1,3 @@
-"""
-    Flux.Losses
-    
-This sub-module contains many loss functions, all of which accept two arguments,
-with the model output as the fist argument: `loss(model(x), y)`.
-It also contains a few related utilities, such as `label_smoothing`.
-The complete list of exports is:
-
-    label_smoothing,
-    mse, mae, msle,
-    crossentropy,
-    logitcrossentropy,
-    binarycrossentropy,
-    logitbinarycrossentropy,
-    kldivergence,
-    huber_loss,
-    tversky_loss,
-    dice_coeff_loss,
-    poisson_loss,
-    hinge_loss,
-    squared_hinge_loss,
-    binary_focal_loss,
-    focal_loss,
-    siamese_contrastive_loss
-"""
 module Losses
 
 using Statistics
