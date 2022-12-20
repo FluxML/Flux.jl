dabd4f53775424f7ead469ab5729a21fd32b0333
josephsdavid <david.josephs@beacon.bio>
2022-12-20 19:05:59 +0000

    implement logit_focal_loss

--- src/losses/functions.jl
+++ src/losses/functions.jl
@@ -604,13 +604,55 @@ function focal_loss(ŷ, y; dims=1, agg=mean, γ=2, ϵ=epseltype(ŷ))
 end
 
 """
+    logit_focal_loss(ŷ, y; dims=1, agg=mean, γ=2, ϵ=eps(ŷ))
+
+Return the [focal_loss](https://arxiv.org/pdf/1708.02002.pdf)
+which can be used in classification tasks with highly imbalanced classes.
+It down-weights well-classified examples and focuses on hard examples.
+The input, 'ŷ', is expected to be normalized (i.e. [softmax](@ref Softmax) output).
+
+The modulating factor, `γ`, controls the down-weighting strength.
+For `γ == 0`, the loss is mathematically equivalent to [`Losses.crossentropy`](@ref).
+
+# Example
+```jldoctest
+julia> y = [1  0  0  0  1
+            0  1  0  1  0
+            0  0  1  0  0]
+3×5 Matrix{Int64}:
+ 1  0  0  0  1
+ 0  1  0  1  0
+ 0  0  1  0  0
+
+julia> ŷ = softmax(reshape(-7:7, 3, 5) .* 1f0)
+3×5 Matrix{Float32}:
+ 0.0900306  0.0900306  0.0900306  0.0900306  0.0900306
+ 0.244728   0.244728   0.244728   0.244728   0.244728
+ 0.665241   0.665241   0.665241   0.665241   0.665241
+
+julia> Flux.logit_focal_loss(ŷ, y) ≈ 1.1277571935622628
+true
+```
+
+See also: [`Losses.binary_focal_loss`](@ref) for binary (not one-hot) labels
+
+"""
+function logit_focal_loss(ŷ, y; γ=2.0f0, agg=mean, dims=1, ϵ=Flux.epseltype(ŷ))
+    Flux.losses._check_sizes(ŷ, y)
+    logpt = logsoftmax(ŷ; dims=dims)
+    logpt .+= ϵ
+    loss = agg(sum(@. -y * (1 - exp.(logpt))^γ * logpt; dims=dims))
+    return loss
+end
+
+"""
     siamese_contrastive_loss(ŷ, y; margin = 1, agg = mean)
-                                    
+
 Return the [contrastive loss](http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf)
 which can be useful for training Siamese Networks. It is given by
-                                    
-    agg(@. (1 - y) * ŷ^2 + y * max(0, margin - ŷ)^2)                           
-                                 
+
+    agg(@. (1 - y) * ŷ^2 + y * max(0, margin - ŷ)^2)
+
 Specify `margin` to set the baseline for distance at which pairs are dissimilar.
 
 # Example
