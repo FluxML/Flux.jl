04d76c65413ab23d58bf7f33ef0d4695aa25aa73
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2023-02-09 23:36:57 -0500

    fixup

--- src/layers/normalise.jl
+++ src/layers/normalise.jl
@@ -194,8 +194,8 @@ function (a::LayerNorm)(x::AbstractArray)
       _size_check(a, x, d => size(a.diag.scale, d))
     end
   end
-  eps = float(eltype(x))(a.ϵ))
-  a.diag(normalise(x, dims=1:length(a.size), ϵ=eps)
+  eps = convert(float(eltype(x)), a.ϵ)  # avoids promotion for Float16 data, but should ε chage too?
+  a.diag(normalise(x, dims=1:length(a.size), ϵ=eps))
 end
 
 function Base.show(io::IO, l::LayerNorm)
