6278916b643d8b1be685a59a2bb5fd6d0d65abd2
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-02-13 09:33:46 +0100

    Revert "remove flatten"
    This reverts commit d91c203d195d820d7000e8a27e3ee5f26b4680a4.

--- src/layers/stateless.jl
+++ src/layers/stateless.jl
@@ -1,4 +1,32 @@
 """
+    flatten(x::AbstractArray)
+
+Reshape arbitrarly-shaped input into a matrix-shaped output,
+preserving the size of the last dimension.
+
+See also [`unsqueeze`](@ref).
+
+# Examples
+```jldoctest
+julia> rand(3,4,5) |> Flux.flatten |> size
+(12, 5)
+
+julia> xs = rand(Float32, 10,10,3,7);
+
+julia> m = Chain(Conv((3,3), 3 => 4, pad=1), Flux.flatten, Dense(400 => 33));
+
+julia> xs |> m[1] |> size
+(10, 10, 4, 7)
+
+julia> xs |> m |> size
+(33, 7)
+```
+"""
+function flatten(x::AbstractArray)
+  return reshape(x, :, size(x)[end])
+end
+
+"""
     normalise(x; dims=ndims(x), ϵ=1e-5)
 
 Normalise `x` to mean 0 and standard deviation 1 across the dimension(s) given by `dims`.
