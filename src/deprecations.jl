d8e1fdd12020fd7e8a9e6a6994428043cb9f6d51
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2023-02-22 06:01:04 +0100

    reintroduce flatten
    fix #2195

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -187,6 +187,8 @@ function update!(opt::Optimise.AbstractOptimiser, ::Params, grads::Union{Tuple, 
     """)
 end
 
+# https://github.com/FluxML/Flux.jl/issues/2195
+const flatten = MLUtils.flatten
 
 # v0.14 deprecations
 
