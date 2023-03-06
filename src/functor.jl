e3df3f65780ec26fe87014c1d6a69cbd9c3021b3
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2023-03-06 08:13:03 +0100

    reintegrate has_cudnn check (#2200)
    * reintegrate has_cudnn check
    
    * typo

--- src/functor.jl
+++ src/functor.jl
@@ -253,12 +253,16 @@ end
 function check_use_cuda()
   if use_cuda[] === nothing
     use_cuda[] = CUDA.functional()
+    if use_cuda[] && !cuDNN.has_cudnn()
+      @warn "CUDA.jl found cuda, but did not find libcudnn. Some functionality will not be available."  maxlog=1
+    end
     if !(use_cuda[])
       @info """The GPU function is being called but the GPU is not accessible. 
                Defaulting back to the CPU. (No action is required if you want to run on the CPU).""" maxlog=1
     end
   end
 end
+
 ChainRulesCore.@non_differentiable check_use_cuda()
 
 # Precision
