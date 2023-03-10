111d8145fb48b3c8a9e30ccd9c66ed3a7e68c51c
CarloLucibello <carlo.lucibello@gmail.com>
2023-03-10 07:04:37 +0100

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,48 +13,48 @@ Random.seed!(0)
 
 @testset verbose=true "Flux.jl" begin
 
-  # @testset "Utils" begin
-  #   include("utils.jl")
-  # end
+  @testset "Utils" begin
+    include("utils.jl")
+  end
 
-  # @testset "Optimise / Train" begin
-  #   include("optimise.jl")
-  #   include("train.jl")
-  # end
+  @testset "Optimise / Train" begin
+    include("optimise.jl")
+    include("train.jl")
+  end
 
-  # @testset "Data" begin
-  #   include("data.jl")
-  # end
+  @testset "Data" begin
+    include("data.jl")
+  end
 
-  # @testset "Losses" begin
-  #   include("losses.jl")
-  #   include("ctc.jl")
-  #   CUDA.functional() && include("ctc-gpu.jl")
-  # end
+  @testset "Losses" begin
+    include("losses.jl")
+    include("ctc.jl")
+    CUDA.functional() && include("ctc-gpu.jl")
+  end
 
-  # @testset "Layers" begin
+  @testset "Layers" begin
     include("layers/attention.jl")
-  #   include("layers/basic.jl")
-  #   include("layers/normalisation.jl")
-  #   include("layers/stateless.jl")
-  #   include("layers/recurrent.jl")
-  #   include("layers/conv.jl")
-  #   include("layers/upsample.jl")
-  #   include("layers/show.jl")
-  # end
+    include("layers/basic.jl")
+    include("layers/normalisation.jl")
+    include("layers/stateless.jl")
+    include("layers/recurrent.jl")
+    include("layers/conv.jl")
+    include("layers/upsample.jl")
+    include("layers/show.jl")
+  end
 
-  # @testset "outputsize" begin
-  #   using Flux: outputsize
-  #   include("outputsize.jl")
-  # end
+  @testset "outputsize" begin
+    using Flux: outputsize
+    include("outputsize.jl")
+  end
 
-  # @testset "CUDA" begin
-  #   if CUDA.functional()
-  #     include("cuda/runtests.jl")
-  #   else
-  #     @warn "CUDA unavailable, not testing GPU support"
-  #   end
-  # end
+  @testset "CUDA" begin
+    if CUDA.functional()
+      include("cuda/runtests.jl")
+    else
+      @warn "CUDA unavailable, not testing GPU support"
+    end
+  end
 
   @static if VERSION == v"1.6"
     using Documenter
