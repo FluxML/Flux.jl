fb507aa41167c481743b5aa1956e5b9697edff32
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2023-07-19 08:37:26 +0200

    delete file accidentally committed

--- cuda.jl
+++
@@ -1,9 +0,0 @@
-using Flux, CUDA
-
-BN = BatchNorm(3) |> gpu;
-x = randn(2, 2, 3, 4) |> gpu;
-
-NNlib.batchnorm(BN.γ, BN.β, x, BN.μ, BN.σ², BN.momentum; 
-                  alpha=1, beta=0, eps=BN.ϵ, 
-                  training=Flux._isactive(BN, x))
-
