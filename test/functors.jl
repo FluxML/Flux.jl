c2744161a470424b4f1769378c773948796e4079
Ian Butterworth <i.r.butterworth@gmail.com>
2023-07-19 11:20:33 -0400

    fix

--- test/functors.jl
+++ test/functors.jl
@@ -1,10 +1,4 @@
 x = rand(Float32, 10, 10)
-if Flux.CUDA_LOADED[]
-    @test x !== gpu(x)
-elseif Flux.AMD_LOADED[]
-    @test x !== gpu(x)
-elseif Flux.METAL_LOADED[]
-    @test x !== gpu(x)
-else
+if !(Flux.CUDA_LOADED[] || Flux.AMD_LOADED[] || Flux.METAL_LOADED[])
     @test x === gpu(x)
 end
