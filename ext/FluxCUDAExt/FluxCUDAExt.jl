95e3bc3b91d198a2a164b6fef9f28758892bcbfc
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-01 00:57:37 +0530

    Setting `deviceID` to `nothing` if GPU is not functional.

--- ext/FluxCUDAExt/FluxCUDAExt.jl
+++ ext/FluxCUDAExt/FluxCUDAExt.jl
@@ -40,7 +40,7 @@ function __init__()
     Flux.CUDA_LOADED[] = true
 
     ## add device to available devices
-    Flux.DEVICES[][Flux.GPU_BACKEND_ORDER["CUDA"]] = Flux.FluxCUDADevice(CUDA.device())
+    Flux.DEVICES[][Flux.GPU_BACKEND_ORDER["CUDA"]] = CUDA.functional() ? Flux.FluxCUDADevice(CUDA.device()) : Flux.FluxCUDADevice(nothing)
 
     try
        Base.require(Main, :cuDNN)
