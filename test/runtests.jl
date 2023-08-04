4815c2251af593da11dc59a64f212e642b35e1c1
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-04 19:38:42 +0530

    Conditionally running CPU tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,53 +11,65 @@ using cuDNN
 # ENV["FLUX_TEST_AMDGPU"] = "true"
 ENV["FLUX_TEST_CUDA"] = "true"
 # ENV["FLUX_TEST_METAL"] = "true"
+# ENV["FLUX_TEST_CPU"] = "false"
 
 include("test_utils.jl")
 
 Random.seed!(0)
 
 @testset verbose=true "Flux.jl" begin
+  if get(ENV, "FLUX_TEST_CPU", "true") == "true"
+    @testset "Utils" begin
+      include("utils.jl")
+    end
 
-  @testset "Utils" begin
-    include("utils.jl")
-  end
+    @testset "Loading" begin
+      include("loading.jl")
+    end
 
-  @testset "Loading" begin
-    include("loading.jl")
-  end
+    @testset "Optimise / Train" begin
+      include("optimise.jl")
+      include("train.jl")
+    end
 
-  @testset "Optimise / Train" begin
-    include("optimise.jl")
-    include("train.jl")
-  end
+    @testset "Data" begin
+      include("data.jl")
+    end
 
-  @testset "Data" begin
-    include("data.jl")
-  end
+    @testset "Losses" begin
+      include("losses.jl")
+      include("ctc.jl")
+    end
 
-  @testset "Losses" begin
-    include("losses.jl")
-    include("ctc.jl")
-  end
+    @testset "Layers" begin
+      include("layers/attention.jl")
+      include("layers/basic.jl")
+      include("layers/normalisation.jl")
+      include("layers/stateless.jl")
+      include("layers/recurrent.jl")
+      include("layers/conv.jl")
+      include("layers/upsample.jl")
+      include("layers/show.jl")
+    end
 
-  @testset "Layers" begin
-    include("layers/attention.jl")
-    include("layers/basic.jl")
-    include("layers/normalisation.jl")
-    include("layers/stateless.jl")
-    include("layers/recurrent.jl")
-    include("layers/conv.jl")
-    include("layers/upsample.jl")
-    include("layers/show.jl")
-  end
+    @testset "outputsize" begin
+      using Flux: outputsize
+      include("outputsize.jl")
+    end
 
-  @testset "outputsize" begin
-    using Flux: outputsize
-    include("outputsize.jl")
-  end
+    @testset "functors" begin
+      include("functors.jl")
+    end
 
-  @testset "functors" begin
-    include("functors.jl")
+    @static if VERSION == v"1.9"
+      using Documenter
+      @testset "Docs" begin
+        DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive=true)
+        doctest(Flux)
+      end
+    end
+  else
+      @info "Skipping CPU tests."
   end
 
   if get(ENV, "FLUX_TEST_CUDA", "false") == "true"
@@ -105,11 +117,4 @@ Random.seed!(0)
     @info "Skipping Metal tests, set FLUX_TEST_METAL=true to run them."
   end
 
-  @static if VERSION == v"1.9"
-    using Documenter
-    @testset "Docs" begin
-      DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive=true)
-      doctest(Flux)
-    end
-  end
 end
