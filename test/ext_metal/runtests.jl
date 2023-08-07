9e5851da989bd812f3a3f391905d2fd88fee411d
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-07 16:50:22 +0200

    Avoid broadcast-related type instabilities with huber_loss (#2306)
    * Avoid broadcast-related type instabilities with huber_loss
    
    * Add test case
    
    * Fix function name
    
    * Fix test

--- test/ext_metal/runtests.jl
+++ test/ext_metal/runtests.jl
@@ -11,3 +11,15 @@ include("test_utils.jl")
 @testset "Basic" begin
     include("basic.jl")
 end
+
+@testset "Huber Loss test" begin
+    X = Flux.gpu(Float32[0,1])
+    Y = Flux.gpu(Float32[1,0])
+
+    grad = Flux.gradient(X, Y) do a,b
+        Flux.Losses.huber_loss(a,b)
+    end
+
+    @test Flux.cpu(grad[1]) == [-0.5, 0.5]
+    @test Flux.cpu(grad[2]) == [0.5, -0.5]
+end
