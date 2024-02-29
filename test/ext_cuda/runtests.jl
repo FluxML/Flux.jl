871dc50277ed409b6ef345b737fecdddec6d53a2
Ian Butterworth <i.r.butterworth@gmail.com>
2024-02-29 13:12:29 -0500

    test log/print sync

--- test/ext_cuda/runtests.jl
+++ test/ext_cuda/runtests.jl
@@ -10,6 +10,11 @@ CUDA.allowscalar(false)
 # include("../test_utils.jl")
 include("test_utils.jl")
 
+for i in 1:100
+  println(i)
+  @info i
+end
+
 @testset "get_devices" begin
   include("get_devices.jl")
 end
