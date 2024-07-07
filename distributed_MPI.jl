181cc9c4001edf7eb0c7ee8d6417d080f3e32d9f
askorupka <agata.skorupka@gmail.com>
2024-07-07 20:16:03 +0200

    feat: add DistributedUtils (MPI&NCCL working)

---
+++ distributed_MPI.jl
@@ -0,0 +1,40 @@
+# =======================================================
+#                      MPI backend
+#                    RUN THIS WITH 
+# `mpiexecjl --project=@. -n 3 julia distributed_MPI.jl`
+# =======================================================
+
+using Flux, MPI, NCCL, CUDA
+using Random
+using Optimisers
+using Zygote
+using Statistics
+
+CUDA.allowscalar(false)
+
+DistributedUtils.initialize(MPIBackend)
+backend = DistributedUtils.get_distributed_backend(MPIBackend)
+rank = DistributedUtils.local_rank(backend)
+
+model = Chain(Dense(1 => 256, tanh), Dense(256 => 1)) |> gpu
+
+model = DistributedUtils.synchronize!!(backend, DistributedUtils.FluxDistributedModel(model); root=0) 
+
+x = rand(Float32, 1, 16) |> gpu
+y = x .^ 3
+
+opt = DistributedUtils.DistributedOptimizer(backend, Optimisers.Adam(0.001f0))
+st_opt = Optimisers.setup(opt, model)
+st_opt = DistributedUtils.synchronize!!(backend, st_opt; root=0) 
+
+loss(model) = mean((model(x) .- y).^2)
+g_ = gradient(m -> loss(m), model)[1] 
+Optimisers.update!(st_opt, model, g_)
+
+for epoch in 1:100
+  global model, st_opt
+  l, back = Zygote.pullback(loss, model)
+  println("Epoch $epoch: Loss $l")
+  g = back(one(l))[1]
+  st_opt, model = Optimisers.update(st_opt, model, g)
+end
