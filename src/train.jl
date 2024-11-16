67e11a3883a889f487c93653424936e41e60c4c6
CarloLucibello <carlo.lucibello@gmail.com>
2024-11-16 20:23:15 +0100

    fix

--- src/train.jl
+++ src/train.jl
@@ -133,15 +133,4 @@ function _rule_to_state(model, rule::Optimisers.AbstractRule)
   state
 end
 
-
-# An easy error to make is to pass result of explicit gradient(...), not gradient(...)[1]
-# Can't catch every case, but can catch many simple Flux models:
-
-function Optimisers.update!(opt, model::Flux.Chain, grads::Tuple)
-  # Zygote will make a NamedTuple{(:layers,)} for the gradient of Chain, Diffractor a Tangent
-  @warn """explicit `update!(opt, model, grad)` wants the gradient for the model alone,
-    not the whole tuple from `gradient(m -> loss(m, x, y), model)`. You probably want `grads[1]`."""
-  return Optimisers.update!(opt, model, grads[1])
-end
-
 end # module Train
