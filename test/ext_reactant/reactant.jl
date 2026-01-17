53ac42e7395f2b0fb45c511c7e62ce0ac1835263
Carlo Lucibello <carlo.lucibello@gmail.com>
2026-01-17 20:10:55 +0100

    fix reactant

--- test/ext_reactant/reactant.jl
+++ test/ext_reactant/reactant.jl
@@ -1,5 +1,5 @@
 @testset "Reactant Models" begin
-    broken_models = (,)
+    broken_models = ()
     for (model, x, name) in TEST_MODELS
         @testset "Reactant grad check $name" begin
             @test test_gradients(model, x; reference=AutoZygote(), test_reactant=true, test_cpu=false)  broken=(name ∈ broken_models)
