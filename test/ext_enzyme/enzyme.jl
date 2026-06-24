57e29baf48edf50c0dd4fc9f027a8900ce3cef66
Carlo Lucibello <carlo.lucibello@unibocconi.it>
2026-06-24 16:09:35 +0200

    unbreak enzyme tests (#2688)
    * unbreak enzyme tests
    
    * deactivate windows
    
    * todo comment

--- test/ext_enzyme/enzyme.jl
+++ test/ext_enzyme/enzyme.jl
@@ -1,16 +1,10 @@
 # ENZYME CPU TESTS
 
-if !Sys.iswindows()
-    @testset "enzyme gradients" begin
-        if VERSION >= v"1.12"
-            BROKEN_TESTS = ["Bilinear", "MultiHeadAttention"]
-        else 
-            BROKEN_TESTS = []
-        end
-        for (model, x, name) in TEST_MODELS
-            @testset "Enzyme grad check $name" begin
-                @test test_gradients(model, x; reference=AutoZygote(), compare=AutoEnzyme()) broken=(name ∈ BROKEN_TESTS)
-            end
+@testset "enzyme gradients" begin
+    BROKEN_TESTS = []
+    for (model, x, name) in TEST_MODELS
+        @testset "Enzyme grad check $name" begin
+            @test test_gradients(model, x; reference=AutoZygote(), compare=AutoEnzyme()) broken=(name ∈ BROKEN_TESTS)
         end
     end
 end
